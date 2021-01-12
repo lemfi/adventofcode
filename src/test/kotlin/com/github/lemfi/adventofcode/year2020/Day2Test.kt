7d97e549c9842acb1d027724a803c1e3ea9969e0
lemfi <lemfi.22@gmail.com>
2021-01-12 20:58:10 +0100

    refacto day2

--- src/test/kotlin/com/github/lemfi/adventofcode/year2020/Day2Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2020/Day2Test.kt
@@ -7,7 +7,7 @@ class Day2Test {
 
     @Test
     fun sample1() {
-        Assertions.assertEquals(2, day2_1("""1-3 a: abcde
+        Assertions.assertEquals(2, Day2.star1("""1-3 a: abcde
             |1-3 b: cdefg
             |2-9 c: ccccccccc""".trimMargin())
         )
@@ -15,9 +15,20 @@ class Day2Test {
 
     @Test
     fun sample2() {
-        Assertions.assertEquals(1, day2_2("""1-3 a: abcde
+        Assertions.assertEquals(1, Day2.star2("""1-3 a: abcde
             |1-3 b: cdefg
             |2-9 c: ccccccccc""".trimMargin())
+        )
+    }
+
+    @Test
+    fun real1() {
+        Assertions.assertEquals(580, Day2.star1(Day2.data))
+    }
+
+    @Test
+    fun real2() {
+        Assertions.assertEquals(611, Day2.star2(Day2.data)
         )
     }
 }
