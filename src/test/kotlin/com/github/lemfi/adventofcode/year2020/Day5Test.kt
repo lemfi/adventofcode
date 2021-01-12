90830fc33bd62a358e9e5652bcced1a0d51101aa
lemfi <lemfi.22@gmail.com>
2021-01-12 22:25:58 +0100

    refacto day5

--- src/test/kotlin/com/github/lemfi/adventofcode/year2020/Day5Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2020/Day5Test.kt
@@ -8,30 +8,42 @@ class Day5Test {
     @Test
     fun sample1() {
 
-        Assertions.assertEquals(567, day5GetSeat("BFFFBBFRRR"))
+        Assertions.assertEquals(567, Day5.getSeat("BFFFBBFRRR"))
     }
 
     @Test
     fun sample2() {
 
-        Assertions.assertEquals(119, day5GetSeat("FFFBBBFRRR"))
+        Assertions.assertEquals(119, Day5.getSeat("FFFBBBFRRR"))
     }
 
     @Test
     fun sample3() {
 
-        Assertions.assertEquals(820, day5GetSeat("BBFFBBFRLL"))
+        Assertions.assertEquals(820, Day5.getSeat("BBFFBBFRLL"))
     }
 
     @Test
     fun sample4() {
 
-        Assertions.assertEquals(1023, day5GetSeat("BBBBBBBRRR"))
+        Assertions.assertEquals(1023, Day5.getSeat("BBBBBBBRRR"))
     }
 
     @Test
     fun sample5() {
 
-        Assertions.assertEquals(0, day5GetSeat("FFFFFFFLLL"))
+        Assertions.assertEquals(0, Day5.getSeat("FFFFFFFLLL"))
+    }
+
+    @Test
+    fun real1() {
+
+        Assertions.assertEquals(908, Day5.star1(Day5.data))
+    }
+
+    @Test
+    fun real2() {
+
+        Assertions.assertEquals(619, Day5.star2(Day5.data))
     }
 }
