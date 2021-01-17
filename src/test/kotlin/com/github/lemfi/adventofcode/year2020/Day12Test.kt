a9d887eb58deef4a27f7ce858d7c16123c9248d2
lemfi <lemfi.22@gmail.com>
2021-01-17 17:51:10 +0100

    refacto day12

--- src/test/kotlin/com/github/lemfi/adventofcode/year2020/Day12Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2020/Day12Test.kt
@@ -8,7 +8,7 @@ class Day12Test {
     @Test
     fun sample1() {
 
-        Assertions.assertEquals(25, day12_1("""F10
+        Assertions.assertEquals(25, Day12.star1("""F10
 N3
 F7
 R90
@@ -18,7 +18,7 @@ F11"""))
     @Test
     fun sample2() {
 
-        Assertions.assertEquals(286, day12_2("""F10
+        Assertions.assertEquals(286, Day12.star2("""F10
 N3
 F7
 R90
@@ -27,45 +27,45 @@ F11"""))
 
     @Test
     fun `waypoint changes as expected`() {
-        val nav = Nav2()
+        val nav = Day12.Nav2()
         nav.command("N", 3).let {
             Assertions.assertEquals(
-                    setOf(Waypoint("E", 10), Waypoint("N", 4), Waypoint("S", 0), Waypoint("W", 0)),
+                    setOf(Day12.Waypoint("E", 10), Day12.Waypoint("N", 4), Day12.Waypoint("S", 0), Day12.Waypoint("W", 0)),
                     it.waypoints.toSet()
             )
         }
 
         nav.command("R", 90).let {
             Assertions.assertEquals(
-                    setOf(Waypoint("E", 4), Waypoint("N", 0), Waypoint("S", 10), Waypoint("W", 0)),
+                    setOf(Day12.Waypoint("E", 4), Day12.Waypoint("N", 0), Day12.Waypoint("S", 10), Day12.Waypoint("W", 0)),
                     it.waypoints.toSet()
             )
         }
 
         nav.command("L", 90).let {
             Assertions.assertEquals(
-                    setOf(Waypoint("E", 10), Waypoint("N", 4), Waypoint("S", 0), Waypoint("W", 0)),
+                    setOf(Day12.Waypoint("E", 10), Day12.Waypoint("N", 4), Day12.Waypoint("S", 0), Day12.Waypoint("W", 0)),
                     it.waypoints.toSet()
             )
         }
 
         nav.command("R", 180).let {
             Assertions.assertEquals(
-                    setOf(Waypoint("W", 10), Waypoint("S", 4), Waypoint("N", 0), Waypoint("E", 0)),
+                    setOf(Day12.Waypoint("W", 10), Day12.Waypoint("S", 4), Day12.Waypoint("N", 0), Day12.Waypoint("E", 0)),
                     it.waypoints.toSet()
             )
         }
 
         nav.command("L", 180).let {
             Assertions.assertEquals(
-                    setOf(Waypoint("E", 10), Waypoint("N", 4), Waypoint("S", 0), Waypoint("W", 0)),
+                    setOf(Day12.Waypoint("E", 10), Day12.Waypoint("N", 4), Day12.Waypoint("S", 0), Day12.Waypoint("W", 0)),
                     it.waypoints.toSet()
             )
         }
 
         nav.command("R", 270).let {
             Assertions.assertEquals(
-                    setOf(Waypoint("N", 10), Waypoint("W", 4), Waypoint("E", 0), Waypoint("S", 0)),
+                    setOf(Day12.Waypoint("N", 10), Day12.Waypoint("W", 4), Day12.Waypoint("E", 0), Day12.Waypoint("S", 0)),
                     it.waypoints.toSet()
             )
         }
@@ -74,7 +74,7 @@ F11"""))
     }
     @Test
     fun `forward changes as expected`() {
-        val nav = Nav2()
+        val nav = Day12.Nav2()
 
         nav.command("F", 10).let {
             Assertions.assertEquals(100, it.east)
@@ -92,12 +92,12 @@ F11"""))
 
     @Test
     fun real1() {
-        Assertions.assertEquals(1838, day12_1(day12data))
+        Assertions.assertEquals(1838, Day12.star1(Day12.data))
     }
 
     @Test
     fun real2() {
-        Assertions.assertEquals(89936, day12_2(day12data))
+        Assertions.assertEquals(89936, Day12.star2(Day12.data))
     }
 
 }
