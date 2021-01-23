80cf9d5e4a6f1246ae6b61fbeb1715f368e540f9
lemfi <lemfi.22@gmail.com>
2021-01-23 15:11:51 +0100

    refact day 14

--- src/test/kotlin/com/github/lemfi/adventofcode/year2020/Day14Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2020/Day14Test.kt
@@ -9,7 +9,7 @@ class Day14Test {
     @Test
     fun sample1() {
 
-        Assertions.assertEquals(165, day14_1("""mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+        Assertions.assertEquals(165, Day14.star1("""mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
 mem[8] = 11
 mem[7] = 101
 mem[8] = 0
@@ -22,7 +22,7 @@ mem[8] = 0"""))
     @Test
     fun sample2() {
 
-        Assertions.assertEquals(208, day14_2("""mask = 000000000000000000000000000000X1001X
+        Assertions.assertEquals(208, Day14.star2("""mask = 000000000000000000000000000000X1001X
 mem[42] = 100
 mask = 00000000000000000000000000000000X0XX
 mem[26] = 1"""))
@@ -30,12 +30,12 @@ mem[26] = 1"""))
 
     @Test
     fun real1() {
-        Assertions.assertEquals(17934269678453, day14_1(day14data))
+        Assertions.assertEquals(17934269678453, Day14.star1(Day14.data))
     }
 
     @Test
     fun real2() {
-        Assertions.assertEquals(3440662844064, day14_2(day14data))
+        Assertions.assertEquals(3440662844064, Day14.star2(Day14.data))
     }
 
 }
