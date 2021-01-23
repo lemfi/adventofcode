1023d17271d5a423c2ad3fce1312f1d4904e7810
lemfi <lemfi.22@gmail.com>
2021-01-23 15:17:19 +0100

    refacto day 15

--- src/main/kotlin/com/github/lemfi/adventofcode/year2020/day15.kt
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2020/day15.kt
@@ -1,62 +1,69 @@
 package com.github.lemfi.adventofcode.year2020
 
-fun day15_1(data: String): Int {
-
-    return data.split(",")
-            .map { it.toInt() }.play(2020)
-}
-
-tailrec fun List<Int>.play(max: Int): Int {
-    return when {
-        this.size == max -> last()
-        indexOf(last()) == size -1 -> (this + listOf(0)).play(max)
-        else -> (this + listOf(size - 1 - dropLast(1).lastIndexOf(last()))).play(max)
-    }
-}
-
-class Brain(
-        val init: List<Int>
-) {
-    var last: Int
-    var vues: MutableMap<Int, List<Int>>
-
-    init {
-        last = init.last()
-        vues = init.mapIndexed { index, i -> i to listOf(index + 1) }.toMap().toMutableMap()
-    }
-
-    fun play(iterations: Int): Int {
-        (this.vues.entries.size + 1 .. iterations).forEach { index ->
-
-            val seen = vues.getOrDefault(last, mutableListOf())
-
-            last = if (seen.size == 2) seen.last() - seen.first() else 0
-
-            val seenlast = vues.getOrDefault(last, mutableListOf())
-            if (seenlast.isNotEmpty()) {
-                vues.put(last, listOf(seenlast.last(), index))
-            } else {
-                vues.put(last, listOf(index))
-            }
-        }
-        return last
-    }
-}
-
-fun day15_2(data: String): Int {
-    return data.split(",")
-            .map { it.toInt() }
-            .let { Brain(it).play(30000000) }
-}
+import java.nio.charset.Charset
 
 fun main() {
 
-    day15_1(day15data)
-            .apply { println("res: $this") }
+    Day15.star1(Day15.data)
+        .apply { println("res: $this") }
 
-    day15_2(day15data)
-            .apply { println("res: $this") }
+    Day15.star2(Day15.data)
+        .apply { println("res: $this") }
 
 }
 
-val day15data = """8,11,0,19,1,2"""
+object Day15 {
+
+    fun star1(data: String): Int {
+
+        return data.split(",")
+            .map { it.toInt() }
+            .play(2020)
+    }
+
+    fun star2(data: String): Int {
+        return data.split(",")
+            .map { it.toInt() }
+            .let { Brain(it).play(30000000) }
+    }
+
+    tailrec fun List<Int>.play(max: Int): Int {
+        return when {
+            this.size == max -> last()
+            indexOf(last()) == size -1 -> (this + listOf(0)).play(max)
+            else -> (this + listOf(size - 1 - dropLast(1).lastIndexOf(last()))).play(max)
+        }
+    }
+
+    class Brain(
+        init: List<Int>
+    ) {
+        var last: Int
+        var vues: MutableMap<Int, List<Int>>
+
+        init {
+            last = init.last()
+            vues = init.mapIndexed { index, i -> i to listOf(index + 1) }.toMap().toMutableMap()
+        }
+
+        fun play(iterations: Int): Int {
+            (this.vues.entries.size + 1 .. iterations).forEach { index ->
+
+                val seen = vues.getOrDefault(last, mutableListOf())
+
+                last = if (seen.size == 2) seen.last() - seen.first() else 0
+
+                val seenlast = vues.getOrDefault(last, mutableListOf())
+                if (seenlast.isNotEmpty()) {
+                    vues.put(last, listOf(seenlast.last(), index))
+                } else {
+                    vues.put(last, listOf(index))
+                }
+            }
+            return last
+        }
+    }
+
+    val data: String by lazy { javaClass.getResourceAsStream("/2020/day15.txt").readAllBytes().toString(Charset.defaultCharset()) }
+
+}
--- src/test/kotlin/com/github/lemfi/adventofcode/year2020/Day15Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2020/Day15Test.kt
@@ -10,95 +10,95 @@ class Day15Test {
     @Test
     fun sample10() {
 
-        Assertions.assertEquals(436, day15_1("""0,3,6"""))
+        Assertions.assertEquals(436, Day15.star1("""0,3,6"""))
     }
 
     @Test
     fun sample11() {
 
-        Assertions.assertEquals(1, day15_1("""1,3,2"""))
+        Assertions.assertEquals(1, Day15.star1("""1,3,2"""))
     }
 
     @Test
     fun sample12() {
 
-        Assertions.assertEquals(10, day15_1("""2,1,3"""))
+        Assertions.assertEquals(10, Day15.star1("""2,1,3"""))
     }
 
     @Test
     fun sample13() {
 
-        Assertions.assertEquals(27, day15_1("""1,2,3"""))
+        Assertions.assertEquals(27, Day15.star1("""1,2,3"""))
     }
 
     @Test
     fun sample14() {
 
-        Assertions.assertEquals(78, day15_1("""2,3,1"""))
+        Assertions.assertEquals(78, Day15.star1("""2,3,1"""))
     }
 
     @Test
     fun sample15() {
 
-        Assertions.assertEquals(438, day15_1("""3,2,1"""))
+        Assertions.assertEquals(438, Day15.star1("""3,2,1"""))
     }
 
     @Test
     fun sample16() {
 
-        Assertions.assertEquals(1836, day15_1("""3,1,2"""))
+        Assertions.assertEquals(1836, Day15.star1("""3,1,2"""))
     }
 
     @Test
     fun sample21() {
 
-        Assertions.assertEquals(2578, day15_2("""1,3,2"""))
+        Assertions.assertEquals(2578, Day15.star2("""1,3,2"""))
     }
 
     @Test
     fun sample22() {
 
-        Assertions.assertEquals(3544142, day15_2("""2,1,3"""))
+        Assertions.assertEquals(3544142, Day15.star2("""2,1,3"""))
     }
 
     @Test
     fun sample23() {
 
-        Assertions.assertEquals(261214, day15_2("""1,2,3"""))
+        Assertions.assertEquals(261214, Day15.star2("""1,2,3"""))
     }
 
     @Test
     fun sample24() {
 
-        Assertions.assertEquals(6895259, day15_2("""2,3,1"""))
+        Assertions.assertEquals(6895259, Day15.star2("""2,3,1"""))
     }
 
     @Test
     fun sample25() {
 
-        Assertions.assertEquals(18, day15_2("""3,2,1"""))
+        Assertions.assertEquals(18, Day15.star2("""3,2,1"""))
     }
 
     @Test
     fun sample26() {
 
-        Assertions.assertEquals(362, day15_2("""3,1,2"""))
+        Assertions.assertEquals(362, Day15.star2("""3,1,2"""))
     }
 
     @Test
     fun sample27() {
 
-        Assertions.assertEquals(175594, day15_2("""0,3,6"""))
+        Assertions.assertEquals(175594, Day15.star2("""0,3,6"""))
     }
 
     @Test
     fun real1() {
-        Assertions.assertEquals(447, day15_1(day15data))
+        Assertions.assertEquals(447, Day15.star1(Day15.data))
     }
 
     @Test
     fun real2() {
-        Assertions.assertEquals(11721679, day15_2(day15data))
+        Assertions.assertEquals(11721679, Day15.star2(Day15.data))
     }
 
 }
