49327f7735a9c08936de6b55569ed98ec5500cd4
lemfi <lemfi.22@gmail.com>
2021-01-23 15:25:36 +0100

    refacto day 17

--- src/main/kotlin/com/github/lemfi/adventofcode/year2020/day17.kt
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2020/day17.kt
@@ -1,8 +1,22 @@
 package com.github.lemfi.adventofcode.year2020
 
-fun day17_1(data: String): Int {
+import java.nio.charset.Charset
 
-    return data
+fun main() {
+
+    Day17.star1(Day17.data)
+        .apply { println("res: $this") }
+
+    Day17.star2(Day17.data)
+        .apply { println("res: $this") }
+
+}
+
+object Day17 {
+
+    fun star1(data: String): Int {
+
+        return data
             .toActive3DCoordinates()
             .toCube()
             .also {
@@ -11,11 +25,11 @@ fun day17_1(data: String): Int {
                 }
             }
             .actives.size
-}
+    }
 
-fun day17_2(data: String): Int {
+    fun star2(data: String): Int {
 
-    return data
+        return data
             .toActive4DCoordinates()
             .toCube()
             .also {
@@ -24,84 +38,69 @@ fun day17_2(data: String): Int {
                 }
             }
             .actives.size
-}
-
-data class Cube(val actives: MutableList<Coordinates>) {
-
-    fun mutate() {
-        val toSwithOff = actives.filter { it.neighboors().count { it.isActive() }.let { it != 2 && it != 3 } }
-        val toSwithOn = actives.flatMap { it.neighboors() }.filterNot { it.isActive() }.groupBy { it }.filter { it.value.size == 3 }
-
-        actives.removeAll(toSwithOff)
-        actives.addAll(toSwithOn.keys)
     }
 
-    private fun Coordinates.isActive() = actives.contains(this)
-}
+    data class Cube(val actives: MutableList<Coordinates>) {
 
-fun String.toActive3DCoordinates() = mutableListOf<`3DCoordinates`>().also { res ->
-    lines().forEachIndexed { y, line ->
-        line.toList().forEachIndexed { x, value ->
-            if (value == '#') res.add(`3DCoordinates`(x, y, 0))
+        fun mutate() {
+            val toSwithOff = actives.filter { it.neighboors().count { it.isActive() }.let { it != 2 && it != 3 } }
+            val toSwithOn = actives.flatMap { it.neighboors() }.filterNot { it.isActive() }.groupBy { it }.filter { it.value.size == 3 }
+
+            actives.removeAll(toSwithOff)
+            actives.addAll(toSwithOn.keys)
         }
+
+        private fun Coordinates.isActive() = actives.contains(this)
     }
-}
 
-fun String.toActive4DCoordinates() = mutableListOf<`4DCoordinates`>().also { res ->
-    lines().forEachIndexed { y, line ->
-        line.toList().forEachIndexed { x, value ->
-            if (value == '#') res.add(`4DCoordinates`(x, y, 0, 0))
-        }
-    }
-}
-
-sealed class Coordinates {
-    abstract fun neighboors(): List<Coordinates>
-}
-
-data class `3DCoordinates`(val x: Int, val y: Int, val z: Int): Coordinates() {
-
-    override fun neighboors() = (x - 1 .. x + 1).flatMap { x ->
-        (y - 1 .. y + 1).flatMap { y ->
-            (z - 1 .. z + 1).flatMap { z -> listOf(`3DCoordinates`(x, y, z)) }
-        }
-    }.filterNot { it == this }
-}
-
-
-data class `4DCoordinates`(val x: Int, val y: Int,  val z: Int, val w: Int): Coordinates() {
-
-    override fun neighboors() = (x - 1 .. x + 1).flatMap { x ->
-        (y - 1 .. y + 1).flatMap { y ->
-            (z - 1 .. z + 1).flatMap { z ->
-                (w - 1 .. w + 1).flatMap { w -> listOf(`4DCoordinates`(x, y, z, w)) }
+    fun String.toActive3DCoordinates() = mutableListOf<Coordinates.`3DCoordinates`>().also { res ->
+        lines().forEachIndexed { y, line ->
+            line.toList().forEachIndexed { x, value ->
+                if (value == '#') res.add(Coordinates.`3DCoordinates`(x, y, 0))
             }
         }
-    }.filterNot { it == this }
+    }
+
+    fun String.toActive4DCoordinates() = mutableListOf<Coordinates.`4DCoordinates`>().also { res ->
+        lines().forEachIndexed { y, line ->
+            line.toList().forEachIndexed { x, value ->
+                if (value == '#') res.add(Coordinates.`4DCoordinates`(x, y, 0, 0))
+            }
+        }
+    }
+
+    sealed class Coordinates {
+        abstract fun neighboors(): List<Coordinates>
+
+        data class `3DCoordinates`(val x: Int, val y: Int, val z: Int): Coordinates() {
+
+            override fun neighboors() = (x - 1 .. x + 1).flatMap { x ->
+                (y - 1 .. y + 1).flatMap { y ->
+                    (z - 1 .. z + 1).flatMap { z -> listOf(`3DCoordinates`(x, y, z)) }
+                }
+            }.filterNot { it == this }
+        }
+
+
+        data class `4DCoordinates`(val x: Int, val y: Int,  val z: Int, val w: Int): Coordinates() {
+
+            override fun neighboors() = (x - 1 .. x + 1).flatMap { x ->
+                (y - 1 .. y + 1).flatMap { y ->
+                    (z - 1 .. z + 1).flatMap { z ->
+                        (w - 1 .. w + 1).flatMap { w -> listOf(`4DCoordinates`(x, y, z, w)) }
+                    }
+                }
+            }.filterNot { it == this }
+        }
+    }
+
+    @Suppress("unchecked_cast")
+    @JvmName("3dToCube")
+    fun MutableList<Coordinates.`3DCoordinates`>.toCube() = Cube(this as MutableList<Coordinates>)
+    @Suppress("unchecked_cast")
+    @JvmName("4dToCube")
+    fun MutableList<Coordinates.`4DCoordinates`>.toCube() = Cube(this as MutableList<Coordinates>)
+
+    val data: String by lazy { javaClass.getResourceAsStream("/2020/day17.txt").readAllBytes().toString(Charset.defaultCharset()) }
+
 }
-
-@Suppress("unchecked_cast")
-@JvmName("3dToCube")
-fun MutableList<`3DCoordinates`>.toCube() = Cube(this as MutableList<Coordinates>)
-@Suppress("unchecked_cast")
-@JvmName("4dToCube")
-fun MutableList<`4DCoordinates`>.toCube() = Cube(this as MutableList<Coordinates>)
-
-fun main() {
-
-    day17_1(day17data)
-            .apply { println("res: $this") }
-
-    day17_2(day17data)
-            .apply { println("res: $this") }
-
-}
-
-val day17data = """.##..#.#
-#...##.#
-##...#.#
-.##.##..
-...#.#.#
-.##.#..#
-...#..##
-###..##."""
--- src/test/kotlin/com/github/lemfi/adventofcode/year2020/Day17Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2020/Day17Test.kt
@@ -9,35 +9,35 @@ class Day17Test {
     fun neighboor() {
 
         val expected = setOf(
-                `3DCoordinates`(-1, 0, 1),
-                `3DCoordinates`(0, 0, 1),
-                `3DCoordinates`(1, 0, 1),
-                `3DCoordinates`(-1, -1, 1),
-                `3DCoordinates`(0, -1, 1),
-                `3DCoordinates`(1, -1, 1),
-                `3DCoordinates`(-1, 1, 1),
-                `3DCoordinates`(0, 1, 1),
-                `3DCoordinates`(1, 1, 1),
-                `3DCoordinates`(-1, 0, 0),
-                `3DCoordinates`(1, 0, 0),
-                `3DCoordinates`(-1, -1, 0),
-                `3DCoordinates`(0, -1, 0),
-                `3DCoordinates`(1, -1, 0),
-                `3DCoordinates`(-1, 1, 0),
-                `3DCoordinates`(0, 1, 0),
-                `3DCoordinates`(1, 1, 0),
-                `3DCoordinates`(-1, 0, -1),
-                `3DCoordinates`(0, 0, -1),
-                `3DCoordinates`(1, 0, -1),
-                `3DCoordinates`(-1, -1, -1),
-                `3DCoordinates`(0, -1, -1),
-                `3DCoordinates`(1, -1, -1),
-                `3DCoordinates`(-1, 1, -1),
-                `3DCoordinates`(0, 1, -1),
-                `3DCoordinates`(1, 1, -1)
+                Day17.Coordinates.`3DCoordinates`(-1, 0, 1),
+                Day17.Coordinates.`3DCoordinates`(0, 0, 1),
+                Day17.Coordinates.`3DCoordinates`(1, 0, 1),
+                Day17.Coordinates.`3DCoordinates`(-1, -1, 1),
+                Day17.Coordinates.`3DCoordinates`(0, -1, 1),
+                Day17.Coordinates.`3DCoordinates`(1, -1, 1),
+                Day17.Coordinates.`3DCoordinates`(-1, 1, 1),
+                Day17.Coordinates.`3DCoordinates`(0, 1, 1),
+                Day17.Coordinates.`3DCoordinates`(1, 1, 1),
+                Day17.Coordinates.`3DCoordinates`(-1, 0, 0),
+                Day17.Coordinates.`3DCoordinates`(1, 0, 0),
+                Day17.Coordinates.`3DCoordinates`(-1, -1, 0),
+                Day17.Coordinates.`3DCoordinates`(0, -1, 0),
+                Day17.Coordinates.`3DCoordinates`(1, -1, 0),
+                Day17.Coordinates.`3DCoordinates`(-1, 1, 0),
+                Day17.Coordinates.`3DCoordinates`(0, 1, 0),
+                Day17.Coordinates.`3DCoordinates`(1, 1, 0),
+                Day17.Coordinates.`3DCoordinates`(-1, 0, -1),
+                Day17.Coordinates.`3DCoordinates`(0, 0, -1),
+                Day17.Coordinates.`3DCoordinates`(1, 0, -1),
+                Day17.Coordinates.`3DCoordinates`(-1, -1, -1),
+                Day17.Coordinates.`3DCoordinates`(0, -1, -1),
+                Day17.Coordinates.`3DCoordinates`(1, -1, -1),
+                Day17.Coordinates.`3DCoordinates`(-1, 1, -1),
+                Day17.Coordinates.`3DCoordinates`(0, 1, -1),
+                Day17.Coordinates.`3DCoordinates`(1, 1, -1)
         )
 
-        val actual = `3DCoordinates`(0, 0, 0).neighboors().toSet()
+        val actual = Day17.Coordinates.`3DCoordinates`(0, 0, 0).neighboors().toSet()
 
         Assertions.assertEquals(expected, actual)
     }
@@ -45,7 +45,7 @@ class Day17Test {
     @Test
     fun sample1() {
 
-        Assertions.assertEquals(112, day17_1(""".#.
+        Assertions.assertEquals(112, Day17.star1(""".#.
 ..#
 ###"""))
     }
@@ -53,19 +53,19 @@ class Day17Test {
     @Test
     fun sample2() {
 
-        Assertions.assertEquals(848, day17_2(""".#.
+        Assertions.assertEquals(848, Day17.star2(""".#.
 ..#
 ###"""))
     }
 
     @Test
     fun real1() {
-        Assertions.assertEquals(359, day17_1(day17data))
+        Assertions.assertEquals(359, Day17.star1(Day17.data))
     }
 
     @Test
     fun real2() {
-        Assertions.assertEquals(2228, day17_2(day17data))
+        Assertions.assertEquals(2228, Day17.star2(Day17.data))
     }
 
 }
