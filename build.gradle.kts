6c67b35d8fa50021f203a52d333842d2fb95acb8
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2021-11-22 21:22:51 +0100

    add kotlin script runtime

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 }
 
 dependencies {
+    implementation(kotlin("script-runtime"))
+
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
@@ -21,6 +23,6 @@ tasks.test {
     useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile>() {
+tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "11"
 }
