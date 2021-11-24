d085d47323e338d3df957090eef2242e83bb41f6
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2021-11-25 00:03:03 +0100

    2015 - day4

---
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2015/Day4Test.kt
@@ -0,0 +1,24 @@
+package com.github.lemfi.adventofcode.year2015
+
+import com.github.lemfi.adventofcode.year2015.Day4.Day4
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Day4Test {
+
+    @Test
+    fun sample1() {
+        Assertions.assertEquals(609043, Day4.star1("abcdef"))
+        Assertions.assertEquals(1048970, Day4.star1("pqrstuv"))
+    }
+
+    @Test
+    fun star1() {
+        Assertions.assertEquals(254575, Day4.star1(null))
+    }
+
+    @Test
+    fun star2() {
+        Assertions.assertEquals(1038736, Day4.star2(null))
+    }
+}
