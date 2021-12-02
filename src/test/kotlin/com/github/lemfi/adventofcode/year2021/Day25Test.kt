4bab8129cfa2927bedf3699565981dfb256f25b2
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2021-12-02 21:21:16 +0100

    add missing day

---
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2021/Day25Test.kt
@@ -0,0 +1,18 @@
+package com.github.lemfi.adventofcode.year2021
+
+import com.github.lemfi.adventofcode.year2021.Day25.Day25
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Day25Test {
+
+    @Test
+    fun star1() {
+        Assertions.assertEquals(0, Day25.star1(null))
+    }
+
+    @Test
+    fun star2() {
+        Assertions.assertEquals(0, Day25.star2(null))
+    }
+}
