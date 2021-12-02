512999d183f98dc7558ea98024051135dc1a8d07
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2021-12-02 21:17:59 +0100

    2021 - day2

--- src/main/kotlin/com/github/lemfi/adventofcode/year2021/Day2.kts
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2021/Day2.kts
@@ -6,9 +6,52 @@ object Day2 {
 
     private fun String?.toData() = this ?: data(2)
 
-    fun star1(input: String?) = 0
+    private fun String?.prepareData() = toData()
+        .lines()
+        .map {
+            it.split(" ").let { (instruction, value) ->
+                when (instruction) {
+                    "forward" -> Forward(value.toInt())
+                    "up" -> Up(value.toInt())
+                    "down" -> Down(value.toInt())
+                    else -> error("hmm...")
+                }
+            }
+        }
 
-    fun star2(input: String?) = 0
+    fun star1(input: String?) = input
+        .prepareData()
+        .fold(0 to 0) { (x, y), instruction ->
+            instruction.resolve1(x, y)
+        }.let { (x, y) -> x * y }
+
+    fun star2(input: String?) = input
+        .prepareData()
+        .fold(Triple(0, 0, 0)) { (x, y, aim), instruction ->
+            instruction.resolve2(x, y, aim)
+        }.let { (x, y) -> x * y }
+
+    sealed class Instruction {
+        abstract val move: Int
+        abstract fun resolve1(x: Int, y: Int): Pair<Int, Int>
+        abstract fun resolve2(x: Int, y: Int, aim: Int): Triple<Int, Int, Int>
+    }
+
+    data class Forward(override val move: Int) : Instruction() {
+        override fun resolve1(x: Int, y: Int) = (x + move) to y
+        override fun resolve2(x: Int, y: Int, aim: Int) = Triple(x + move, y - aim * move, aim)
+    }
+
+    data class Up(override val move: Int) : Instruction() {
+        override fun resolve1(x: Int, y: Int) = x to (y - move)
+        override fun resolve2(x: Int, y: Int, aim: Int) = Triple(x, y, aim + move)
+    }
+
+    data class Down(override val move: Int) : Instruction() {
+        override fun resolve1(x: Int, y: Int) = x to (y + move)
+        override fun resolve2(x: Int, y: Int, aim: Int) = Triple(x, y, aim - move)
+    }
+
 }
 
 processStars(Day2::star1, Day2::star2)
--- src/test/kotlin/com/github/lemfi/adventofcode/year2021/Day2Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2021/Day2Test.kt
@@ -8,11 +8,37 @@ class Day2Test {
 
     @Test
     fun star1() {
-        Assertions.assertEquals(0, Day2.star1(null))
+        Assertions.assertEquals(
+            150, Day2.star1(
+                """forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2"""
+            )
+        )
     }
 
     @Test
     fun star2() {
-        Assertions.assertEquals(0, Day2.star2(null))
+        Assertions.assertEquals(
+            900, Day2.star2(
+                """forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2"""
+            )
+        )
     }
+
+    @Test
+    fun puzzle1() =
+        Assertions.assertEquals(1451208, Day2.star1(null))
+
+    @Test
+    fun puzzle2() =
+        Assertions.assertEquals(1620141160, Day2.star2(null))
 }
