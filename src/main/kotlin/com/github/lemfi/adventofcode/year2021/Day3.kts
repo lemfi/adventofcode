161d47b44537205152f7b387a0f7e4d5bafb374d
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2021-12-04 14:20:27 +0100

    2021 - day3

--- src/main/kotlin/com/github/lemfi/adventofcode/year2021/Day3.kts
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2021/Day3.kts
@@ -6,9 +6,58 @@ object Day3 {
 
     private fun String?.toData() = this ?: data(3)
 
-    fun star1(input: String?) = 0
+    private fun String.occurence(selector: (Int, Int) -> Char) = lines().let { lines ->
+        (0 until lines.first().length).map { id ->
+            lines.map { it[id] }
+        }
+    }
+        .map { line -> selector(line.count { it == '0' }, line.count { it == '1' }) }
+        .joinToString("")
 
-    fun star2(input: String?) = 0
+    private fun String.gamma() = occurence { c0, c1 ->
+        if (c0 > c1) '0' else '1'
+    }
+
+    private fun String.epsilon() = occurence { c0, c1 ->
+        if (c0 <= c1) '0' else '1'
+    }
+
+    private fun String.gamma(index: Int) = gamma().toList()[index]
+
+    private fun String.epsilon(index: Int) = epsilon().toList()[index]
+
+    fun star1(input: String?) = input.toData().let {
+        it.gamma().toInt(2) * it.epsilon().toInt(2)
+    }
+
+    fun star2(input: String?) = input.toData().let { numbers ->
+        oxygen(numbers) * co2(numbers)
+    }
+
+    private fun oxygen(numbers: String): Int {
+        return numbers.bitCriteria { epsilon(it) }
+    }
+
+    private fun co2(numbers: String): Int {
+        return numbers.bitCriteria { gamma(it) }
+    }
+
+    private fun String.bitCriteria(compute: String.(Int) -> Char): Int {
+        return lines()
+            .toMutableList()
+            .also { lines ->
+                (0..lines.first().length).map { index ->
+                    if (lines.size > 1) {
+                        with(
+                            lines.joinToString("\n")
+                                .compute(index)
+                        ) {
+                            lines.removeIf { it[index] == this }
+                        }
+                    }
+                }
+            }.joinToString("").toInt(2)
+    }
 }
 
 processStars(Day3::star1, Day3::star2)
--- src/test/kotlin/com/github/lemfi/adventofcode/year2021/Day3Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2021/Day3Test.kt
@@ -8,11 +8,51 @@ class Day3Test {
 
     @Test
     fun star1() {
-        Assertions.assertEquals(0, Day3.star1(null))
+        Assertions.assertEquals(
+            198, Day3.star1(
+                """00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010"""
+            )
+        )
     }
 
     @Test
     fun star2() {
-        Assertions.assertEquals(0, Day3.star2(null))
+        Assertions.assertEquals(
+            230, Day3.star2(
+                """00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010"""
+            )
+        )
+    }
+
+    @Test
+    fun puzzle1() {
+        Assertions.assertEquals(4118544, Day3.star1(null))
+    }
+
+    @Test
+    fun puzzle2() {
+        Assertions.assertEquals(3832770, Day3.star2(null))
     }
 }
