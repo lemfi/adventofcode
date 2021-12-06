006f1e77f13fec90c6f8c039408bc311fdfaeda1
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2021-12-06 23:48:19 +0100

    upgrade junit version

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,8 +15,8 @@ dependencies {
     implementation(kotlin("script-runtime"))
 
     testImplementation(kotlin("test-junit5"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
 tasks.test {
