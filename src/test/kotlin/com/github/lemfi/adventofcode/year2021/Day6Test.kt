d859cfc66359b4d94a26c529a66e9caa171c3a59
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2021-12-06 21:26:50 +0100

    2021 - day6

--- src/test/kotlin/com/github/lemfi/adventofcode/year2021/Day6Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2021/Day6Test.kt
@@ -8,11 +8,21 @@ class Day6Test {
 
     @Test
     fun star1() {
-        Assertions.assertEquals(0, Day6.star1(null))
+        Assertions.assertEquals(5934, Day6.star1("""3,4,3,1,2"""))
     }
 
     @Test
     fun star2() {
-        Assertions.assertEquals(0, Day6.star2(null))
+        Assertions.assertEquals(26984457539, Day6.star2("""3,4,3,1,2"""))
+    }
+
+    @Test
+    fun puzzle1() {
+        Assertions.assertEquals(359999, Day6.star1(null))
+    }
+
+    @Test
+    fun puzzle2() {
+        Assertions.assertEquals(1631647919273, Day6.star2(null))
     }
 }
