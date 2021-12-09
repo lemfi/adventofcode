5c06434e5df4d6d6edc7af4721770c71797586e2
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2021-12-09 22:16:05 +0100

    2021 - day9 :white_check_mark:

--- src/main/kotlin/com/github/lemfi/adventofcode/year2021/Day9.kts
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2021/Day9.kts
@@ -6,9 +6,61 @@ object Day9 {
 
     private fun String?.toData() = this ?: data(9)
 
-    fun star1(input: String?) = 0
+    data class Element(val x: Int, val y: Int, val height: Int)
 
-    fun star2(input: String?) = 0
+    private fun String?.prepareData() = toData().lines().let { lines ->
+        (lines.indices).flatMap { y ->
+            (lines.first().indices).map { x ->
+                Element(x, y, lines[y].toList()[x].toString().toInt())
+            }
+        }
+    }
+
+    fun star1(input: String?) = input
+        .prepareData()
+        .findLowPoints()
+        .sumOf { it.height + 1 }
+
+
+    fun star2(input: String?) = input.prepareData()
+        .run {
+            findLowPoints()
+                .map { `basin size of`(it) }
+                .sorted()
+                .takeLast(3)
+                .reduce { acc, size -> acc * size }
+        }
+
+    private infix fun List<Element>.of(element: Element) = filter {
+        (it.x == element.x + 1 && it.y == element.y)
+                || (it.x == element.x - 1 && it.y == element.y)
+                || (it.x == element.x && it.y == element.y + 1)
+                || (it.x == element.x && it.y == element.y - 1)
+    }
+
+    private infix fun List<Element>.`have a greater height than`(element: Element) = all { it.height > element.height }
+
+    private fun List<Element>.findLowPoints() = let { neighbors ->
+        fold(mutableListOf<Element>()) { acc, element ->
+            acc.apply {
+
+                if (neighbors of element `have a greater height than` element) add(element)
+
+            }
+        }
+    }
+
+    private fun List<Element>.`basin size of`(element: Element): Int = let { neighbors ->
+        mutableSetOf<Element>().apply {
+            mutableListOf(element).also { checkNeighbors ->
+                while (checkNeighbors.isNotEmpty()) {
+                    (neighbors of checkNeighbors.removeFirst())
+                        .filter { it.height != 9 && add(it) }
+                        .apply { checkNeighbors.addAll(this) }
+                }
+            }
+        }.size
+    }
 }
 
 processStars(Day9::star1, Day9::star2)
--- src/test/kotlin/com/github/lemfi/adventofcode/year2021/Day9Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2021/Day9Test.kt
@@ -8,11 +8,37 @@ class Day9Test {
 
     @Test
     fun star1() {
-        Assertions.assertEquals(0, Day9.star1(null))
+        Assertions.assertEquals(
+            15, Day9.star1(
+                """2199943210
+3987894921
+9856789892
+8767896789
+9899965678"""
+            )
+        )
     }
 
     @Test
     fun star2() {
-        Assertions.assertEquals(0, Day9.star2(null))
+        Assertions.assertEquals(
+            1134, Day9.star2(
+                """2199943210
+3987894921
+9856789892
+8767896789
+9899965678"""
+            )
+        )
+    }
+
+    @Test
+    fun puzzle1() {
+        Assertions.assertEquals(496, Day9.star1(null))
+    }
+
+    @Test
+    fun puzzle2() {
+        Assertions.assertEquals(902880, Day9.star2(null))
     }
 }
