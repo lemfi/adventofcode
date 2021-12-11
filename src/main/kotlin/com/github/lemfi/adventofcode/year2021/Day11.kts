22b80ffb19b81272a7faa7afdd8d6ff0e03d9e0b
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2021-12-11 16:58:28 +0100

    2021 - day11 :bulb:

--- src/main/kotlin/com/github/lemfi/adventofcode/year2021/Day11.kts
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2021/Day11.kts
@@ -6,9 +6,75 @@ object Day11 {
 
     private fun String?.toData() = this ?: data(11)
 
-    fun star1(input: String?) = 0
+    private fun String?.prepareData() = toData().lines().let { lines ->
+        (lines.indices).flatMap { y ->
+            (lines.first().indices).map { x ->
+                Octopus(x, y, lines[y][x].toString().toInt())
+            }
+        }
+    }
 
-    fun star2(input: String?) = 0
+    fun star1(input: String?) = input
+        .prepareData()
+        .let { octopuses ->
+            (0 until 100).sumOf {
+                octopuses.increaseEnergyAndDoTheMagic()
+            }
+        }
+
+    fun star2(input: String?) = input
+        .prepareData()
+        .let { octopuses ->
+            var iter = 0
+            while (octopuses.any { it.value != 0 }) {
+                octopuses.increaseEnergyAndDoTheMagic()
+                iter++
+            }
+            iter
+        }
+
+    private fun List<Octopus>.increaseEnergyAndDoTheMagic() =
+        with(toMutableList()) {
+            val flashed = increaseEnergy().flashing()
+            while (flashed.isNotEmpty()) {
+                flashPropagation(flashed)
+            }
+            this@increaseEnergyAndDoTheMagic.size - size
+        }
+
+    data class Octopus(val x: Int, val y: Int, var value: Int)
+
+    private fun Collection<Octopus>.increaseEnergy() = onEach { it.value += 1 }
+
+    private fun Collection<Octopus>.flashing() =
+        filter { it.value > 9 }
+            .onEach { it.value = 0 }
+            .toMutableList()
+
+    private fun MutableList<Octopus>.flashPropagation(flashed: MutableList<Octopus>) {
+        removeAll(flashed)
+        increaseNeighborsEnergy(flashed)
+            .flashing()
+            .also { justFlashed ->
+                flashed.clear()
+                flashed.addAll(justFlashed)
+            }
+    }
+
+    private fun Collection<Octopus>.increaseNeighborsEnergy(octopus: Collection<Octopus>) = apply {
+        octopus.onEach { neighbors(it).increaseEnergy() }
+    }
+
+    private fun Collection<Octopus>.neighbors(octopus: Octopus) = filter {
+        (it.x == octopus.x + 1 && it.y == octopus.y)
+                || (it.x == octopus.x - 1 && it.y == octopus.y)
+                || (it.x == octopus.x && it.y == octopus.y + 1)
+                || (it.x == octopus.x && it.y == octopus.y - 1)
+                || (it.x == octopus.x + 1 && it.y == octopus.y - 1)
+                || (it.x == octopus.x + 1 && it.y == octopus.y + 1)
+                || (it.x == octopus.x - 1 && it.y == octopus.y - 1)
+                || (it.x == octopus.x - 1 && it.y == octopus.y + 1)
+    }
 }
 
 processStars(Day11::star1, Day11::star2)
--- src/test/kotlin/com/github/lemfi/adventofcode/year2021/Day11Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2021/Day11Test.kt
@@ -8,11 +8,47 @@ class Day11Test {
 
     @Test
     fun star1() {
-        Assertions.assertEquals(0, Day11.star1(null))
+        Assertions.assertEquals(
+            1656, Day11.star1(
+                """5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526"""
+            )
+        )
     }
 
     @Test
     fun star2() {
-        Assertions.assertEquals(0, Day11.star2(null))
+        Assertions.assertEquals(
+            195, Day11.star2(
+                """5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526"""
+            )
+        )
+    }
+
+    @Test
+    fun puzzle1() {
+        Assertions.assertEquals(1571, Day11.star1(null))
+    }
+
+    @Test
+    fun puzzle2() {
+        Assertions.assertEquals(387, Day11.star2(null))
     }
 }
