52ee67e50910e5cf80fd48066e58f7a8fe9f8e0b
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2021-12-13 22:55:50 +0100

    2021 - day12 :green_heart:

--- src/test/kotlin/com/github/lemfi/adventofcode/year2021/Day13Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2021/Day13Test.kt
@@ -8,11 +8,82 @@ class Day13Test {
 
     @Test
     fun star1() {
-        Assertions.assertEquals(0, Day13.star1(null))
+        Assertions.assertEquals(
+            17, Day13.star1(
+                """6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5"""
+            )
+        )
     }
 
     @Test
     fun star2() {
-        Assertions.assertEquals(0, Day13.star2(null))
+        Assertions.assertEquals(
+            """
+#####
+#   #
+#   #
+#   #
+#####""", Day13.star2(
+                """6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5"""
+            )
+        )
+    }
+
+    @Test
+    fun puzzle1() {
+        Assertions.assertEquals(666, Day13.star1(null))
+    }
+
+    @Test
+    fun puzzle2() {
+        Assertions.assertEquals(
+            """
+ ##    ## #  #  ##  #### #  # #  # #  #
+#  #    # #  # #  #    # #  # # #  #  #
+#       # #### #  #   #  #### ##   #  #
+#       # #  # ####  #   #  # # #  #  #
+#  # #  # #  # #  # #    #  # # #  #  #
+ ##   ##  #  # #  # #### #  # #  #  ## """, Day13.star2(null)
+        )
     }
 }
