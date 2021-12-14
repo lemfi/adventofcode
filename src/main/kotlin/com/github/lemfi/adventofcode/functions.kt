b6cabbf2ff7b60b2aaa9d484467f38e8e56b753f
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2021-12-14 18:42:00 +0100

    colorize output

--- src/main/kotlin/com/github/lemfi/adventofcode/functions.kt
+++ src/main/kotlin/com/github/lemfi/adventofcode/functions.kt
@@ -5,14 +5,20 @@ import kotlin.time.measureTimedValue
 
 fun String?.data(data: String) = this ?: data
 
+const val ANSI_RESET = "\u001B[0m"
+
+private fun yellow(any: Any) = "\u001B[33m$any$ANSI_RESET"
+private fun blue(any: Any) = "\u001B[34m$any$ANSI_RESET"
+
+
 @OptIn(ExperimentalTime::class)
 fun processStars(vararg star: (String?) -> Any) =
 
     star
         .map { measureTimedValue { it(null) } }
         .mapIndexed { index, timedValue ->
-            """star${index + 1}: 
-                |    res:  ${timedValue.value}
+            """${yellow("star${index + 1}")}: 
+                |    res:  ${blue(timedValue.value)}
                 |    time: ${timedValue.duration}
             """.trimMargin()
         }
