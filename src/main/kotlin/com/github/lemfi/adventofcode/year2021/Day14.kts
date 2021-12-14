213e8f9f2cb4bbe0b26f390c2b42a287f84ec29b
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2021-12-14 18:43:12 +0100

    2021 - day14 :rocket:

--- src/main/kotlin/com/github/lemfi/adventofcode/year2021/Day14.kts
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2021/Day14.kts
@@ -6,9 +6,59 @@ object Day14 {
 
     private fun String?.toData() = this ?: data(14)
 
-    fun star1(input: String?) = 0
+    private fun String?.prepareData() = toData().lines().let { line ->
+        (line.first().let { it.toPairs() to it.toList().last() }).let { (pairs, lastChar) ->
+            Triple(pairs, lastChar, line.drop(2).map { it.split(" -> ").run { first() to last() } })
+        }
+    }
 
-    fun star2(input: String?) = 0
+    fun star1(input: String?) = input.prepareData()
+        .let { (polymerPairs, lastChar, patterns) ->
+            polymerPairs.grow(patterns, 10).toResult(lastChar)
+        }
+
+    fun star2(input: String?) = input.prepareData()
+        .let { (polymerPairs, lastChar, patterns) ->
+            polymerPairs.grow(patterns, 40).toResult(lastChar)
+        }
+
+    private fun MutableMap<String, Long>.grow(
+        patterns: List<Pair<String, String>>,
+        steps: Int
+    ) = apply {
+
+        (1..steps).forEach { _ ->
+            filter { it.value > 0 }.toMutableMap().let { polymerPairs ->
+                polymerPairs.forEach { (pattern, count) ->
+                    val newChar = patterns.first { it.first == pattern }.second
+
+                    increment(pattern, -count)
+                    increment("${pattern.first()}$newChar", count)
+                    increment("$newChar${pattern.last()}", count)
+                }
+            }
+        }
+    }
+
+    private fun Map<String, Long>.toResult(lastChar: Char) = filter { it.value > 0 }
+        .map { (pattern, count) -> pattern.first() to count }
+        .fold(mutableMapOf<Char, Long>()) { acc, (c, count) -> acc.apply { increment(c, count) } }
+        .apply { increment(lastChar, 1) }
+        .run { maxOf { it.value } - minOf { it.value } }
+
+    private fun String.toPairs() = toList()
+        .let { chars ->
+            chars.foldIndexed(mutableListOf<String>()) { index, acc, c ->
+                acc.apply { if (index < length - 1) add("$c${chars[index + 1]}") }
+            }
+        }
+        .groupBy { it }
+        .map { it.key to it.value.size.toLong() }
+        .toMap().toMutableMap()
+
+    private fun <T> MutableMap<T, Long>.increment(pattern: T, inc: Long) {
+        this[pattern] = getOrDefault(pattern, 0L) + inc
+    }
 }
 
 processStars(Day14::star1, Day14::star2)
--- src/test/kotlin/com/github/lemfi/adventofcode/year2021/Day14Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2021/Day14Test.kt
@@ -8,11 +8,63 @@ class Day14Test {
 
     @Test
     fun star1() {
-        Assertions.assertEquals(0, Day14.star1(null))
+        Assertions.assertEquals(
+            1588, Day14.star1(
+                """NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C"""
+            )
+        )
     }
 
     @Test
     fun star2() {
-        Assertions.assertEquals(0, Day14.star2(null))
+        Assertions.assertEquals(
+            2188189693529L, Day14.star2(
+                """NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C"""
+            )
+        )
+    }
+
+    @Test
+    fun puzzle1() {
+        Assertions.assertEquals(3143, Day14.star1(null))
+    }
+
+    @Test
+    fun puzzle2() {
+        Assertions.assertEquals(4110215602456L, Day14.star2(null))
     }
 }
