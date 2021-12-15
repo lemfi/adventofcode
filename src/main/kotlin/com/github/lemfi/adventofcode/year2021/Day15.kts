d4770688009fde22924caa40ea46c4af0afa8188
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2021-12-16 00:15:21 +0100

    2021 - day15 :construction:

--- src/main/kotlin/com/github/lemfi/adventofcode/year2021/Day15.kts
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2021/Day15.kts
@@ -1,14 +1,85 @@
 package com.github.lemfi.adventofcode.year2021
 
 import com.github.lemfi.adventofcode.processStars
+import kotlin.math.min
 
 object Day15 {
 
     private fun String?.toData() = this ?: data(15)
 
-    fun star1(input: String?) = 0
+    private fun String?.prepareData() = toData().lines().let { lines ->
+        (lines.indices).flatMap { y ->
+            (lines[y].indices).map { x ->
+                Point(x, y, lines[y][x].digitToInt())
+            }
+        }
+    }
 
-    fun star2(input: String?) = 0
+    fun star1(input: String?) = input
+        .prepareData()
+        .shortestDistance()
+
+    fun star2(input: String?) = input
+        .prepareData()
+        .expand()
+        .shortestDistance()
+
+    private fun List<Point>.shortestDistance(): Int {
+        val grid = toMutableList()
+        val lastPoint = grid.last()
+
+        var point = grid.first()
+        var distance = 0
+        while (point != lastPoint) {
+            grid
+                .neighbors(point)
+                .onEach { it.guess = min(it.guess, distance + it.risk) }
+
+            point = grid.minByOrNull { it.guess }!!.apply {
+                distance = guess
+                visited = true
+                grid.remove(this)
+            }
+        }
+        return distance
+    }
+
+    fun Collection<Point>.neighbors(point: Point) =
+        filter {
+            !it.visited
+                    && (
+                    it.x == point.x - 1 && it.y == point.y
+                            || it.x == point.x + 1 && it.y == point.y
+                            || it.x == point.x && it.y == point.y - 1
+                            || it.x == point.x && it.y == point.y + 1
+                    )
+        }
+
+    fun List<Point>.expand(): List<Point> {
+        val width = maxOf { it.x } + 1
+        val height = maxOf { it.y } + 1
+
+        return (0..4).flatMap { iterx ->
+            (0..4).flatMap { itery ->
+                map {
+                    Point(
+                        it.x + width * iterx,
+                        it.y + height * itery,
+                        ((it.risk + iterx + itery) % 9).let { if (it == 0) 9 else it }
+                    )
+                }
+            }
+        }
+    }
+
+    data class Point(
+        val x: Int,
+        val y: Int,
+        val risk: Int,
+        var visited: Boolean = false,
+        var guess: Int = Int.MAX_VALUE
+    )
+
 }
 
 processStars(Day15::star1, Day15::star2)
--- src/test/kotlin/com/github/lemfi/adventofcode/year2021/Day15Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2021/Day15Test.kt
@@ -8,11 +8,37 @@ class Day15Test {
 
     @Test
     fun star1() {
-        Assertions.assertEquals(0, Day15.star1(null))
+        Assertions.assertEquals(
+            40, Day15.star1(
+                """1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581"""
+            )
+        )
     }
 
     @Test
     fun star2() {
-        Assertions.assertEquals(0, Day15.star2(null))
+        Assertions.assertEquals(
+            315, Day15.star2(
+                """1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581"""
+            )
+        )
     }
 }
