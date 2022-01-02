fa0d94030433f05456035aa36e62b15de2e1c49e
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2022-01-02 17:43:23 +0100

    2021 - day21 :technologist:

--- src/test/kotlin/com/github/lemfi/adventofcode/year2021/Day21Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2021/Day21Test.kt
@@ -8,11 +8,31 @@ class Day21Test {
 
     @Test
     fun star1() {
-        Assertions.assertEquals(0, Day21.star1(null))
+        Assertions.assertEquals(
+            739785, Day21.star1(
+                """Player 1 starting position: 4
+Player 2 starting position: 8"""
+            )
+        )
     }
 
     @Test
     fun star2() {
-        Assertions.assertEquals(0, Day21.star2(null))
+        Assertions.assertEquals(
+            444356092776315L, Day21.star2(
+                """Player 1 starting position: 4
+Player 2 starting position: 8"""
+            )
+        )
+    }
+
+    @Test
+    fun puzzle1() {
+        Assertions.assertEquals(1196172, Day21.star1(null))
+    }
+
+    @Test
+    fun puzzle2() {
+        Assertions.assertEquals(106768284484217L, Day21.star2(null))
     }
 }
