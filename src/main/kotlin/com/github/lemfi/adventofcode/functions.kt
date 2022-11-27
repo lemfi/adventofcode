207f3732bacb71aacb315fccc5c7c39e3e26d982
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2022-11-27 11:29:43 +0100

    change output of processStars function

--- src/main/kotlin/com/github/lemfi/adventofcode/functions.kt
+++ src/main/kotlin/com/github/lemfi/adventofcode/functions.kt
@@ -9,17 +9,49 @@ const val ANSI_RESET = "\u001B[0m"
 
 private fun yellow(any: Any) = "\u001B[33m$any$ANSI_RESET"
 private fun blue(any: Any) = "\u001B[34m$any$ANSI_RESET"
+private fun underline(any: Any) = "\u001B[4m$any$ANSI_RESET"
 
 
 @OptIn(ExperimentalTime::class)
-fun processStars(vararg star: (String?) -> Any) =
+fun processStars(vararg stars: (String?) -> Any) =
 
-    star
-        .map { measureTimedValue { it(null) } }
-        .mapIndexed { index, timedValue ->
-            """${yellow("star${index + 1}")}: 
-                |    res:  ${blue(timedValue.value)}
-                |    time: ${timedValue.duration}
-            """.trimMargin()
+    measureTimedValue {
+
+        if (stars.size == 2) {
+            stars
+                .forEachIndexed { index, function ->
+                    val timedValue = measureTimedValue { function(null) }
+                    println(
+                        """${yellow("star${index + 1}")}: 
+                                |    res:  ${blue(timedValue.value)}
+                                |    time: ${timedValue.duration}
+                            """.trimMargin()
+                    )
+                }
+        } else {
+            stars
+                .toList()
+                .windowed(2, 2)
+                .forEachIndexed { dayIndex, day ->
+                    println("\n${underline("Day ${dayIndex + 1}")}")
+                    day.forEachIndexed { starIndex, star ->
+                        val timedValue = measureTimedValue { star(null) }
+                        println(
+                            """    ${yellow("star${starIndex + 1}")}: 
+                                    |        res:  ${blue(timedValue.value)}
+                                    |        time: ${timedValue.duration}
+                                """.trimMargin()
+                        )
+                    }
+                }
         }
-        .joinToString("\n\n")
+    }.let {
+        println(
+            """
+                
+                
+                ${underline("TOTAL time:")}
+                ${it.duration}
+            """.trimIndent()
+        )
+    }
