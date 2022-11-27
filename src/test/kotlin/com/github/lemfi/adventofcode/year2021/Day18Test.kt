26787db6fb792afe04e1255bcf3459afa561a441
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2022-11-27 11:32:43 +0100

    fixup day18 kts to kt

--- src/test/kotlin/com/github/lemfi/adventofcode/year2021/Day18Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2021/Day18Test.kt
@@ -1,6 +1,5 @@
 package com.github.lemfi.adventofcode.year2021
 
-import com.github.lemfi.adventofcode.year2021.Day18.Day18
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
