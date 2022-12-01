f0dd9c7eb0e5b336def34603b98e4fe0072b7ccb
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2022-12-01 07:31:47 +0100

    2022 - day 1 :rocket:

--- src/main/kotlin/com/github/lemfi/adventofcode/year2022/Day1.kt
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2022/Day1.kt
@@ -1,15 +1,28 @@
 package com.github.lemfi.adventofcode.year2022
 
-import com.github.lemfi.adventofcode.data
 import com.github.lemfi.adventofcode.processStars
 
 object Day1 {
 
-    private fun String?.toData() = this ?: data("1")
+    private fun String?.toData() = this ?: data(1)
 
-    fun star1(input: String?) = 0
+    fun star1(input: String?) = input
+        .toElfes()
+        .maxOf { elfe -> elfe.supply() }
 
-    fun star2(input: String?) = 0
+    fun star2(input: String?) = input
+        .toElfes()
+        .map { elfe -> elfe.supply() }
+        .sortedDescending()
+        .take(3)
+        .sum()
+
+    private fun String?.toElfes() = toData().split("\n\n")
+
+    private fun String.supply() =
+        split("\n")
+            .map { it.toInt() }
+            .reduce { acc, value -> acc + value }
 }
 
 fun main() {
--- src/test/kotlin/com/github/lemfi/adventofcode/year2022/Day1Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2022/Day1Test.kt
@@ -7,11 +7,45 @@ class Day1Test {
 
     @Test
     fun star1() {
-        Assertions.assertEquals(0, Day1.star1(null))
+        Assertions.assertEquals(
+            24000, Day1.star1(
+                """1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"""
+            )
+        )
     }
 
     @Test
     fun star2() {
-        Assertions.assertEquals(0, Day1.star2(null))
+        Assertions.assertEquals(
+            45000, Day1.star2(
+                """1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"""
+            )
+        )
     }
 }
