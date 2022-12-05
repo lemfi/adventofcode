53a65b6e638f44409ec96ce43e1bc2c3eef6a668
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2022-12-05 17:28:29 +0100

    2022 - day 5 :poop:

--- src/main/kotlin/com/github/lemfi/adventofcode/year2022/Day5.kt
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2022/Day5.kt
@@ -6,9 +6,72 @@ object Day5 {
 
     private fun String?.toData() = this ?: data(5)
 
-    fun star1(input: String?) = 0
+    fun star1(input: String?) = input
+        .moveCrates { (amount, origin, destination), stacks ->
+            repeat(amount) {
+                stacks[origin]
+                    .removeFirst()
+                    .also { crate -> stacks[destination].add(0, crate) }
+            }
+        }
 
-    fun star2(input: String?) = 0
+
+    fun star2(input: String?) = input
+        .moveCrates { (amount, origin, destination), stacks ->
+            (amount - 1 downTo 0).forEach { y ->
+                stacks[origin]
+                    .removeAt(y)
+                    .also { crate -> stacks[destination].add(0, crate) }
+            }
+        }
+
+    private fun String?.moveCrates(mover: (Triple<Int, Int, Int>, List<MutableList<Char>>) -> Unit) =
+        prepareData()
+            .let { (stacks, moves) ->
+                moves.forEach { move -> mover(move, stacks) }
+                stacks.tops()
+            }
+
+    private fun String?.prepareData() =
+        toData()
+            .split("\n\n")
+            .let { (crates, moves) ->
+                computeStacks(crates) to computeMoves(moves)
+            }
+
+    private fun computeStacks(stacks: String) =
+        stacks
+            .lines()
+            .dropLast(1)
+            .let { ords ->
+
+                (1..(ords.first().length + 1) / 4)
+                    .map { mutableListOf<Char>() } // create empty stacks
+
+                    .also { stacks ->
+                        ords.forEach { abs ->
+                            abs.forEachIndexed { x, crate ->
+                                if (crate != ' ' && (x - 1) % 4 == 0) { // crates are at positions 1, 5, 9, ...
+                                    stacks[(x - 1) / 4].add(crate) // fill stack if needed
+                                }
+                            }
+                        }
+                    }
+            }
+
+    private fun computeMoves(moves: String) = moves.lines()
+        .map {
+            it
+                .split(" ")
+                .mapNotNull { runCatching { it.toInt() }.getOrNull() }
+                .let { (amount, origin, destination) ->
+                    Triple(amount, origin - 1, destination - 1)
+                }
+        }
+
+    private fun List<List<Char>>.tops() =
+        joinToString("") { it.first().toString() }
+
 }
 
 fun main() {
--- src/test/kotlin/com/github/lemfi/adventofcode/year2022/Day5Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2022/Day5Test.kt
@@ -7,11 +7,35 @@ class Day5Test {
 
     @Test
     fun star1() {
-        Assertions.assertEquals(0, Day5.star1(null))
+        Assertions.assertEquals(
+            "CMZ", Day5.star1(
+                """    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2"""
+            )
+        )
     }
 
     @Test
     fun star2() {
-        Assertions.assertEquals(0, Day5.star2(null))
+        Assertions.assertEquals(
+            "MCD", Day5.star2(
+                """    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2"""
+            )
+        )
     }
 }
