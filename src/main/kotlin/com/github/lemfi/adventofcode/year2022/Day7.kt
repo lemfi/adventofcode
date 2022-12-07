6444d58cf25e2e4739dd12e1e8370783c84521c2
Le Montreer Fiona <fiona.lemontreer@orange.com>
2022-12-07 09:16:34 +0100

    2022 - day 7 :hammer:

--- src/main/kotlin/com/github/lemfi/adventofcode/year2022/Day7.kt
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2022/Day7.kt
@@ -6,9 +6,70 @@ object Day7 {
 
     private fun String?.toData() = this ?: data(7)
 
-    fun star1(input: String?) = 0
+    fun star1(input: String?) = input
+        .prepare()
+        .analyzeDirs()
+        .let { dirs ->
+            dirs
+                .map { it.size(dirs) }
+                .filter { it <= 100000 }
+                .sum()
+        }
 
-    fun star2(input: String?) = 0
+    fun star2(input: String?) = input
+        .prepare()
+        .analyzeDirs()
+        .let { dirs ->
+            with(dirs.first { it.name == "/" }.size(dirs) + 30000000 - 70000000) {
+                dirs
+                    .map { it.size(dirs) }
+                    .filter { it > this }
+                    .min()
+            }
+        }
+
+    private fun List<Line>.analyzeDirs(): MutableSet<Dir> {
+
+        var currentDir: Dir? = Dir("/")
+        return mutableSetOf(currentDir!!).also { dirs ->
+
+            forEach { line ->
+                when (line) {
+                    is File -> currentDir?.files?.add(line)
+                    is Goto -> currentDir =
+                        if (line.dir == "..") {
+                            currentDir?.parent
+                        } else {
+                            Dir(line.dir, currentDir).apply { dirs.add(this) }
+                        }
+                }
+            }
+        }
+    }
+
+    private fun String?.prepare() = toData()
+        .lines()
+        .drop(1)
+        .mapNotNull {
+            when {
+                it.startsWith("$ cd") -> Goto(it.substringAfter("cd "))
+                !it.startsWith("$") && !it.startsWith("dir") -> File(it.split(" ").first().toInt())
+                else -> null
+            }
+        }
+
+
+    sealed interface Line
+    data class Goto(val dir: String) : Line
+    data class File(val size: Int) : Line
+
+    data class Dir(val name: String, val parent: Dir? = null) {
+
+        val files: MutableList<File> = mutableListOf()
+
+        fun size(dirs: Collection<Dir>): Int =
+            files.sumOf { it.size } + dirs.filter { it.parent == this }.sumOf { it.size(dirs) }
+    }
 }
 
 fun main() {
--- src/test/kotlin/com/github/lemfi/adventofcode/year2022/Day7Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2022/Day7Test.kt
@@ -7,11 +7,55 @@ class Day7Test {
 
     @Test
     fun star1() {
-        Assertions.assertEquals(0, Day7.star1(null))
+        Assertions.assertEquals(95437, Day7.star1("""${'$'} cd /
+${'$'} ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+${'$'} cd a
+${'$'} ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+${'$'} cd e
+${'$'} ls
+584 i
+${'$'} cd ..
+${'$'} cd ..
+${'$'} cd d
+${'$'} ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k"""))
     }
 
     @Test
     fun star2() {
-        Assertions.assertEquals(0, Day7.star2(null))
+        Assertions.assertEquals(24933642, Day7.star2("""${'$'} cd /
+${'$'} ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+${'$'} cd a
+${'$'} ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+${'$'} cd e
+${'$'} ls
+584 i
+${'$'} cd ..
+${'$'} cd ..
+${'$'} cd d
+${'$'} ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k"""))
     }
 }
