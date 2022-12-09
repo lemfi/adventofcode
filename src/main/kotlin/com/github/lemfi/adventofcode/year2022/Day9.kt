0523715da1a9874bf8073f5a922f4c2a048208b3
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2022-12-09 16:04:07 +0100

    2022 - day 9 :chart_with_upwards_trend:

--- src/main/kotlin/com/github/lemfi/adventofcode/year2022/Day9.kt
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2022/Day9.kt
@@ -1,14 +1,120 @@
 package com.github.lemfi.adventofcode.year2022
 
 import com.github.lemfi.adventofcode.processStars
+import kotlin.math.abs
 
 object Day9 {
 
     private fun String?.toData() = this ?: data(9)
 
-    fun star1(input: String?) = 0
+    fun star1(input: String?) = input
+        .readInstruction()
+        .fold(mutableListOf(Position(0, 0) to Position(0, 0))) { ropeStates, movementInstruction ->
+            ropeStates.apply {
+                add(
+                    movementInstruction.move(ropeStates.last().first)
+                        .let { head -> head to ropeStates.last().second.move(head) }
+                )
+            }
+        }.map { (_, tail) -> tail }.toSet().size
 
-    fun star2(input: String?) = 0
+    fun star2(input: String?) = input
+        .readInstruction()
+        .fold(mutableListOf((1..10).map { Position(0, 0) })) { ropeStates, movementInstruction ->
+
+            ropeStates.apply {
+                add(
+                    movementInstruction.move(ropeStates.last().first()).let { H ->
+                        mutableListOf(H).apply {
+                            ropeStates.last().drop(1).forEach { add(it.move(last())) }
+                        }
+                    })
+            }
+
+        }.map { it.last() }.toSet().size
+
+    fun Position.move(head: Position): Position {
+
+        return when {
+            isTouching(head) -> this
+            isOnTopOf(head) -> Down.move()
+            isOnBottomOf(head) -> Up.move()
+            isOnLeftOf(head) -> Right.move()
+            isOnRightOf(head) -> Left.move()
+            isDiagDownLeftOf(head) -> DiagUpRight.move()
+            isDiagUpLeftOf(head) -> DiagDownRight.move()
+            isDiagUpRightOf(head) -> DiagDownLeft.move()
+            isDiagDownRightOf(head) -> DiagUpLeft.move()
+            else -> error("did I forget one case?")
+        }
+
+    }
+    
+    data class Position(val x: Int, val y: Int) {
+        fun Move.move() = move(this@Position)
+        fun isTouching(head: Position) = abs(x - head.x) <= 1 && abs(y - head.y) <= 1
+        fun isOnTopOf(head: Position) = x == head.x && y > head.y
+        fun isOnBottomOf(head: Position) = x == head.x && y < head.y
+        fun isOnLeftOf(head: Position) = x < head.x && y == head.y
+        fun isOnRightOf(head: Position) = x > head.x && y == head.y
+        fun isDiagDownLeftOf(head: Position) = x < head.x && y < head.y
+        fun isDiagUpLeftOf(head: Position) = x < head.x && y > head.y
+        fun isDiagUpRightOf(head: Position) = x > head.x && y > head.y
+        fun isDiagDownRightOf(head: Position) = x > head.x && y < head.y
+    }
+
+    sealed interface Move {
+        fun move(position: Position): Position
+    }
+
+    object Right : Move {
+        override fun move(position: Position) = Position(position.x + 1, position.y)
+    }
+
+    object Left : Move {
+        override fun move(position: Position) = Position(position.x - 1, position.y)
+    }
+
+    object Up : Move {
+        override fun move(position: Position) = Position(position.x, position.y + 1)
+    }
+
+    object Down : Move {
+        override fun move(position: Position) = Position(position.x, position.y - 1)
+    }
+
+    object DiagUpRight : Move {
+        override fun move(position: Position) = Position(position.x + 1, position.y + 1)
+    }
+
+    object DiagUpLeft : Move {
+        override fun move(position: Position) = Position(position.x - 1, position.y + 1)
+    }
+
+    object DiagDownRight : Move {
+        override fun move(position: Position) = Position(position.x + 1, position.y - 1)
+    }
+
+    object DiagDownLeft : Move {
+        override fun move(position: Position) = Position(position.x - 1, position.y - 1)
+    }
+
+    private fun String?.readInstruction() =
+        toData()
+            .lines()
+            .flatMap {
+                it.split(" ").let { (m, n) ->
+                    (1..n.toInt()).map {
+                        when (m) {
+                            "L" -> Left
+                            "D" -> Down
+                            "R" -> Right
+                            "U" -> Up
+                            else -> error("wtf is that instruction?")
+                        }
+                    }
+                }
+            }
 }
 
 fun main() {
--- src/test/kotlin/com/github/lemfi/adventofcode/year2022/Day9Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2022/Day9Test.kt
@@ -7,11 +7,33 @@ class Day9Test {
 
     @Test
     fun star1() {
-        Assertions.assertEquals(0, Day9.star1(null))
+        Assertions.assertEquals(
+            13, Day9.star1(
+                """R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2"""
+            )
+        )
     }
 
     @Test
     fun star2() {
-        Assertions.assertEquals(0, Day9.star2(null))
+        Assertions.assertEquals(
+            36, Day9.star2(
+                """R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20"""
+            )
+        )
     }
 }
