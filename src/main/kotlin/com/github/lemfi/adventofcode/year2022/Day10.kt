7b9eee1e3e47ae917cf1036435f3a6b74bd2120e
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2022-12-10 14:14:55 +0100

    2022 - day 10 :lipstick:

--- src/main/kotlin/com/github/lemfi/adventofcode/year2022/Day10.kt
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2022/Day10.kt
@@ -6,9 +6,41 @@ object Day10 {
 
     private fun String?.toData() = this ?: data(10)
 
-    fun star1(input: String?) = 0
+    fun star1(input: String?) = input
+        .readOperations()
+        .foldIndexed(1 to 0) { index, (X, sum), operation ->
+            operation.compute(X) to if ((index + 1) in 20..220 step 40) sum + X * (index + 1) else sum
+        }.second
 
-    fun star2(input: String?) = 0
+    fun star2(input: String?) = input
+        .readOperations()
+        .foldIndexed(1 to "") { index, (X, output), operation ->
+            operation.compute(X) to if (index % 40 in X - 1..X + 1) "$output#" else "$output "
+        }.second
+        .windowed(40, 40)
+        .joinToString("\n")
+        .let {
+            "\n$it" // because my output is shifted on first line, nothing to do with resolution, just for display ^^
+        }
+
+    private fun String?.readOperations() = toData()
+        .lines()
+        .flatMap {
+            if (it == "noop") listOf(Noop)
+            else listOf(Noop, Addx(it.split(" ").last().toInt()))
+        }
+
+    sealed interface Operation {
+        fun compute(x: Int): Int
+    }
+
+    object Noop : Operation {
+        override fun compute(x: Int) = x
+    }
+
+    data class Addx(val value: Int) : Operation {
+        override fun compute(x: Int) = x + value
+    }
 }
 
 fun main() {
