e36c8e33685f4670454dd54ec978185818b073b6
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2022-12-11 12:27:43 +0100

    2022 - day 11 :bulb:

--- src/main/kotlin/com/github/lemfi/adventofcode/year2022/Day11.kt
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2022/Day11.kt
@@ -6,10 +6,88 @@ object Day11 {
 
     private fun String?.toData() = this ?: data(11)
 
-    fun star1(input: String?) = 0
+    fun star1(input: String?) = input
+        .toMonkeys()
+        .observeMonkeys(1..20) { it / 3 }
 
-    fun star2(input: String?) = 0
+    fun star2(input: String?) = input
+        .toMonkeys()
+        .let { monkeys ->
+            val modulo = monkeys.fold(1L) { acc, m -> acc * m.div }
+            monkeys.observeMonkeys(1..10000) { it % modulo }
+        }
+
+    private fun List<Monkey>.observeMonkeys(turns: IntRange, worryDiminution: (Long) -> Long) = apply {
+        turns
+            .forEach { _ ->
+                forEach { monkey ->
+                    monkey.items.forEach { item ->
+                        val newWorry = worryDiminution(monkey.operation.compute(item))
+                        val newMonkey = monkey.test(newWorry)
+                        get(newMonkey).items.add(newWorry)
+                    }
+                    monkey.inspected += monkey.items.size
+                    monkey.items.clear()
+                }
+            }
+    }
+        .sortedByDescending { it.inspected }
+        .take(2)
+        .let { (m1, m2) -> m1.inspected * m2.inspected }
+
+    data class Monkey(
+        val name: String,
+        val items: MutableList<Long>,
+        val div: Long,
+        val operation: Operation,
+        val trueMonkey: String,
+        val falseMonkey: String,
+        var inspected: Long = 0,
+    ) {
+
+        fun test(item: Long) = if (item % div == 0L) trueMonkey else falseMonkey
+    }
+
+    fun List<Monkey>.get(name: String) = first { it.name == name }
+
+    sealed interface Operation {
+        fun compute(old: Long): Long
+    }
+
+    data class Plus(val operand: Long?) : Operation {
+        override fun compute(old: Long) = (operand?.let { old + operand } ?: (old + old))
+    }
+
+    data class Mult(val operand: Long?) : Operation {
+        override fun compute(old: Long) = (operand?.let { old * operand } ?: (old * old))
+    }
+
+    private fun String?.toMonkeys() =
+        toData()
+            .split("\n\n").map {
+                it.lines().let { (name, starting, operation, test, ifTrue, ifFalse) ->
+
+                    Monkey(
+                        name = name.substringBefore(":").uppercase(),
+                        items = starting.substringAfter("Starting items: ").split(", ").map { it.toLong() }
+                            .toMutableList(),
+                        operation = operation.substringAfter("old ").let {
+                            when (it.first()) {
+                                '*' -> Mult(runCatching { it.substringAfter("* ").toLong() }.getOrNull())
+                                '+' -> Plus(runCatching { it.substringAfter("+ ").toLong() }.getOrNull())
+                                else -> error("wtf is that operation?")
+                            }
+                        },
+                        div = test.substringAfter("Test: divisible by ").toLong(),
+                        trueMonkey = ifTrue.substringAfter("throw to ").uppercase(),
+                        falseMonkey = ifFalse.substringAfter("throw to ").uppercase(),
+                    )
+                }
+            }
+
+    private operator fun <E> List<E>.component6(): E = get(5)
 }
+
 
 fun main() {
     processStars(Day11::star1, Day11::star2)
--- src/test/kotlin/com/github/lemfi/adventofcode/year2022/Day11Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2022/Day11Test.kt
@@ -7,11 +7,71 @@ class Day11Test {
 
     @Test
     fun star1() {
-        Assertions.assertEquals(0, Day11.star1(null))
+        Assertions.assertEquals(
+            10605, Day11.star1(
+                """Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1"""
+            )
+        )
     }
 
     @Test
     fun star2() {
-        Assertions.assertEquals(0, Day11.star2(null))
+        Assertions.assertEquals(
+            2713310158, Day11.star2(
+                """Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1"""
+            )
+        )
     }
 }
