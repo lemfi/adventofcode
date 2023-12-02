78f23873aed57736c9a5f93102cbc0aba9ec3d25
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2023-12-02 09:36:05 +0100

    2023 - day 02 :dizzy:

--- src/main/kotlin/com/github/lemfi/adventofcode/year2023/Day02.kt
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2023/Day02.kt
@@ -6,9 +6,54 @@ object Day02 {
 
     private fun String?.toData() = this ?: data(2)
 
-    fun star1(input: String?) = 0
+    fun star1(input: String?) = input
+        .toData()
+        .lines()
+        .toGames()
+        .filter { game ->
+            game.sets.all { it.red <= 12 && it.green <= 13 && it.blue <= 14 }
+        }
+        .sumOf { it.id }
 
-    fun star2(input: String?) = 0
+    fun star2(input: String?) = input
+        .toData()
+        .lines()
+        .toGames()
+        .sumOf { game ->
+            game.sets.maxOf { it.red } * game.sets.maxOf { it.green } * game.sets.maxOf { it.blue }
+        }
+
+    private fun List<String>.toGames() = map { line ->
+        Game(
+            id = line.substringAfter("Game ").substringBefore(":").toLong(),
+            sets = line.substringAfter(":").split(";").map { set ->
+                set.split(",").let { colors ->
+                    RGB(
+                        red = colors
+                            .firstOrNull { it.contains("red") }
+                            ?.substringBefore(" red")
+                            ?.trim()
+                            ?.toLong()
+                            ?: 0,
+                        blue = colors
+                            .firstOrNull { it.contains("blue") }
+                            ?.substringBefore(" blue")
+                            ?.trim()?.toLong()
+                            ?: 0,
+                        green = colors
+                            .firstOrNull { it.contains("green") }
+                            ?.substringBefore(" green")
+                            ?.trim()
+                            ?.toLong()
+                            ?: 0
+                    )
+                }
+            }
+        )
+    }
+
+    private data class Game(val id: Long, val sets: List<RGB>)
+    private data class RGB(val red: Long, val green: Long, val blue: Long)
 }
 
 fun main() {
--- src/test/kotlin/com/github/lemfi/adventofcode/year2023/Day02Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2023/Day02Test.kt
@@ -7,11 +7,27 @@ class Day02Test {
 
     @Test
     fun star1() {
-        Assertions.assertEquals(0, Day02.star1(null))
+        Assertions.assertEquals(
+            8, Day02.star1(
+                """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
+            )
+        )
     }
 
     @Test
     fun star2() {
-        Assertions.assertEquals(0, Day02.star2(null))
+        Assertions.assertEquals(
+            2286, Day02.star2(
+                """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
+            )
+        )
     }
 }
