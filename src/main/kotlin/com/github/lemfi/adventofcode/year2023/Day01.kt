885fc28311d6c707bdbdffad30f193a84b5a64da
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2023-12-02 09:34:09 +0100

    2023 - day 01 :rocket:

--- src/main/kotlin/com/github/lemfi/adventofcode/year2023/Day01.kt
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2023/Day01.kt
@@ -6,9 +6,52 @@ object Day01 {
 
     private fun String?.toData() = this ?: data(1)
 
-    fun star1(input: String?) = 0
+    fun star1(input: String?) = input
+        .toData()
+        .lines()
+        .sumOf { line ->
+            "${line.first { it.findStar1() }}${line.last { it.findStar1() }}".toInt()
+        }
 
-    fun star2(input: String?) = 0
+    fun star2(input: String?) = input
+        .toData()
+        .lines()
+        .sumOf { line ->
+            "${line.findStar2 { label }}${line.reversed().findStar2 { label.reversed() }}".toInt()
+        }
+
+    private fun Char.findStar1() =
+        runCatching {
+            toString().toInt()
+        }.getOrNull() != null
+
+
+    private fun String.findStar2(transform: Numbers.() -> String) =
+        (indices).firstNotNullOf { index ->
+            Numbers.entries.firstOrNull { substring(index).startsWith(it.transform()) }
+        }.value
+}
+
+private enum class Numbers(val label: String, val value: Int) {
+
+    one("one", 1),
+    two("two", 2),
+    three("three", 3),
+    four("four", 4),
+    five("five", 5),
+    six("six", 6),
+    seven("seven", 7),
+    eight("eight", 8),
+    nine("nine", 9),
+    onen("1", 1),
+    twon("2", 2),
+    threen("3", 3),
+    fourn("4", 4),
+    fiven("5", 5),
+    sixn("6", 6),
+    sevenn("7", 7),
+    eightn("8", 8),
+    ninen("9", 9)
 }
 
 fun main() {
--- src/test/kotlin/com/github/lemfi/adventofcode/year2023/Day01Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2023/Day01Test.kt
@@ -7,11 +7,28 @@ class Day01Test {
 
     @Test
     fun star1() {
-        Assertions.assertEquals(0, Day01.star1(null))
+        Assertions.assertEquals(
+            142, Day01.star1(
+                """1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet"""
+            )
+        )
     }
 
     @Test
     fun star2() {
-        Assertions.assertEquals(0, Day01.star2(null))
+        Assertions.assertEquals(
+            281, Day01.star2(
+                """two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"""
+            )
+        )
     }
 }
