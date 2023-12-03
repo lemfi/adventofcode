21eefce934c9c147e23909fc8ebf434edc7e15a3
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2023-12-03 16:50:20 +0100

    2023 - day 03 :construction_worker:

--- src/main/kotlin/com/github/lemfi/adventofcode/year2023/Day03.kt
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2023/Day03.kt
@@ -6,9 +6,93 @@ object Day03 {
 
     private fun String?.toData() = this ?: data(3)
 
-    fun star1(input: String?) = 0
+    fun star1(input: String?) = input
+        .toData()
+        .lines()
+        .toEngineData { this != '.' }
+        .let { (numbers, symbols) ->
+            numbers
+                .filter { it.touches(symbols) }
+                .map { it.number }
+        }
+        .sum()
 
-    fun star2(input: String?) = 0
+    fun star2(input: String?) = input
+        .toData()
+        .lines()
+        .toEngineData { this == '*' }
+        .let { (numbers, symbols) ->
+            numbers
+                .asSequence()
+                .mapNotNull { it.gear(symbols)?.run { it.number to this } }
+                .groupBy { (_, gear) -> gear }
+                .filter { (_, numbers) -> numbers.size == 2 }
+                .map { (_, numbers) -> numbers.map { (number, _) -> number } }
+                .sumOf { (one, two) -> one * two }
+        }
+
+    private fun List<EngineData?>.analyzeEngine() =
+        asSequence()
+            .filterIsInstance<EngineNumber>()
+            .groupBy { it.position.y }
+            .map { (_, points) ->
+                points.fold(mutableListOf<EngineNumber>()) { res, point ->
+                    val last = res.lastOrNull()
+                    if (last != null && last.position.x + 1 == point.position.x) {
+                        res
+                            .dropLast(1)
+                            .toMutableList()
+                            .apply { add(EngineNumber(point.position, last.number.concat(point.number))) }
+                    } else {
+                        res.apply {
+                            add(point)
+                        }
+                    }
+                }
+            }
+            .flatten()
+            .toList() to filterIsInstance<EngineSymbol>()
+            .map { it.position }
+
+    private fun Long.concat(l: Long) = "$this$l".toLong()
+    private fun Long.length() = toString().length
+
+    private sealed interface EngineData
+
+    private data class EngineNumber(val position: Position, val number: Long) : EngineData
+    private data class EngineSymbol(val position: Position, val symbol: Char) : EngineData
+
+    private fun List<String>.toEngineData(symbol: Char.() -> Boolean) =
+        mapIndexed { y, line ->
+            line.mapIndexed { x, c ->
+                c.tryLong()?.let { EngineNumber(Position(x.toLong(), y.toLong()), it) }
+                    ?: if (c.symbol()) EngineSymbol(Position(x.toLong(), y.toLong()), c) else null
+            }
+        }
+            .flatten()
+            .analyzeEngine()
+
+    private fun Char.tryLong() = runCatching { toString().toLong() }.getOrNull()
+    private fun EngineNumber.touches(symbols: List<Position>) = (position.x - number.length() + 1..position.x)
+        .map { Position(it, position.y) }
+        .any { it.touches(symbols) }
+
+    private fun Position.touches(symbols: List<Position>) = symbols.any {
+        y in (it.y - 1..it.y + 1) && x in (it.x - 1..it.x + 1)
+    }
+
+    private fun EngineNumber.gear(symbols: List<Position>) = (position.x - number.length() + 1..position.x)
+        .map { Position(it, position.y) }
+        .flatMap { it.gear(symbols) }
+        .toSet()
+        .ifEmpty { null }
+
+    private fun Position.gear(symbols: List<Position>) = symbols.filter {
+        y in (it.y - 1..it.y + 1) && x in (it.x - 1..it.x + 1)
+    }
+
+    private data class Position(val x: Long, val y: Long)
+
 }
 
 fun main() {
--- src/test/kotlin/com/github/lemfi/adventofcode/year2023/Day03Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2023/Day03Test.kt
@@ -7,11 +7,37 @@ class Day03Test {
 
     @Test
     fun star1() {
-        Assertions.assertEquals(0, Day03.star1(null))
+        Assertions.assertEquals(
+            4361, Day03.star1(
+                """467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...${'$'}.*....
+.664.598.."""
+            )
+        )
     }
 
     @Test
     fun star2() {
-        Assertions.assertEquals(0, Day03.star2(null))
+        Assertions.assertEquals(
+            467835, Day03.star2(
+                """467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...${'$'}.*....
+.664.598.."""
+            )
+        )
     }
 }
