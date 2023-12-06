98767ab64d9a7d4e69ff6bf1dc2233d83d6c29b7
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2023-12-06 21:23:29 +0100

    2023 - day 06 :zap:

--- src/main/kotlin/com/github/lemfi/adventofcode/year2023/Day06.kt
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2023/Day06.kt
@@ -6,9 +6,26 @@ object Day06 {
 
     private fun String?.toData() = this ?: data(6)
 
-    fun star1(input: String?) = 0
+    fun star1(input: String?) = input
+        .toData()
+        .lines()
+        .compute()
 
-    fun star2(input: String?) = 0
+    fun star2(input: String?) = input
+        .toData()
+        .lines()
+        .map { it.replace(" ", "") }
+        .compute()
+
+    private fun List<String>.compute() =
+        map { Regex("""\d+""").findAll(it).map { it.value.toLong() }.toList() }
+            .filterNot { it.isEmpty() }
+            .let { (l1, l2) -> l1.zip(l2) }
+            .map { (time, distance) ->
+                (0 until time).count { hold ->
+                    hold * (time - hold) > distance
+                }
+            }.reduce { acc, i -> acc * i }
 }
 
 fun main() {
--- src/test/kotlin/com/github/lemfi/adventofcode/year2023/Day06Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2023/Day06Test.kt
@@ -7,11 +7,21 @@ class Day06Test {
 
     @Test
     fun star1() {
-        Assertions.assertEquals(0, Day06.star1(null))
+        Assertions.assertEquals(
+            288, Day06.star1(
+                """Time:      7  15   30
+Distance:  9  40  200"""
+            )
+        )
     }
 
     @Test
     fun star2() {
-        Assertions.assertEquals(0, Day06.star2(null))
+        Assertions.assertEquals(
+            71503, Day06.star2(
+                """Time:      7  15   30
+Distance:  9  40  200"""
+            )
+        )
     }
 }
