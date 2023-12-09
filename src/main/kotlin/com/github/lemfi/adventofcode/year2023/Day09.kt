1776a70307dcdc0b67143372b1a0b27b673ebe67
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2023-12-09 16:08:56 +0100

    2023 - day 09 :boom:

--- src/main/kotlin/com/github/lemfi/adventofcode/year2023/Day09.kt
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2023/Day09.kt
@@ -6,9 +6,40 @@ object Day09 {
 
     private fun String?.toData() = this ?: data(9)
 
-    fun star1(input: String?) = 0
+    fun star1(input: String?) = input
+        .toData()
+        .lines()
+        .sumOf { it.toNextVal() }
 
-    fun star2(input: String?) = 0
+    fun star2(input: String?) = input
+        .toData()
+        .lines()
+        .sumOf { it.toPrevVal() }
+
+    private fun String.toNextVal() = toSteps()
+        .reduce { prev, step ->
+            step.toMutableList()
+                .apply { add(step.last() + prev.last()) }
+
+        }.last()
+
+    private fun String.toPrevVal() = toSteps()
+        .reduce { prev, step ->
+            step.toMutableList()
+                .apply { add(0, step.first() - prev.first()) }
+        }.first()
+
+    private fun String.toSteps() =
+        Regex("""-?\d+""").findAll(this).map { it.value.toLong() }.toList()
+            .let { initial ->
+                val steps = mutableListOf(initial)
+                var source = initial
+                do {
+                    source = source.windowed(2, 1).map { (one, two) -> two - one }
+                    steps.add(0, source)
+                } while (!source.all { it == 0L })
+                steps
+            }
 }
 
 fun main() {
--- src/test/kotlin/com/github/lemfi/adventofcode/year2023/Day09Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2023/Day09Test.kt
@@ -7,11 +7,23 @@ class Day09Test {
 
     @Test
     fun star1() {
-        Assertions.assertEquals(0, Day09.star1(null))
+        Assertions.assertEquals(
+            114, Day09.star1(
+                """0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"""
+            )
+        )
     }
 
     @Test
     fun star2() {
-        Assertions.assertEquals(0, Day09.star2(null))
+        Assertions.assertEquals(
+            2, Day09.star2(
+                """0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"""
+            )
+        )
     }
 }
