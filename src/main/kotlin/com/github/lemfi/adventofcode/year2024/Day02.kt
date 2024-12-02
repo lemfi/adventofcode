5de2ecb78483890bf8d49886f9fd1a3129ed6e04
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2024-12-02 20:30:34 +0100

    2024 - day 02 :tada:

--- src/main/kotlin/com/github/lemfi/adventofcode/year2024/Day02.kt
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2024/Day02.kt
@@ -6,9 +6,28 @@ object Day02 {
 
     private fun String?.toData() = this ?: data(2)
 
-    fun star1(input: String?) = 0
+    fun star1(input: String?) = input
+        .parse()
+        .count { it.isValid() }
 
-    fun star2(input: String?) = 0
+    fun star2(input: String?) = input
+        .parse()
+        .count { level ->
+            level.indices.sortedDescending().any { level.toMutableList().apply { removeAt(it) }.isValid() }
+        }
+
+    private fun List<Int>.isValid() =
+        (sorted() == this || sortedDescending() == this) &&
+                sorted()
+                    .let { numbers ->
+                        numbers.indices.all {
+                            runCatching { numbers[it + 1] in (numbers[it] + 1..numbers[it] + 3) }.getOrElse { true }
+                        }
+                    }
+
+    private fun String?.parse() = toData()
+        .lines()
+        .map { it.split(" ").map { s -> s.toInt() } }
 }
 
 fun main() {
