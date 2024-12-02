9f6c793da761a79bb1cfdca6c82f75200224762f
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2024-12-02 20:30:34 +0100

    2024 - day 02 :tada:

--- src/test/kotlin/com/github/lemfi/adventofcode/year2024/Day02Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2024/Day02Test.kt
@@ -7,11 +7,29 @@ class Day02Test {
 
     @Test
     fun star1() {
-        Assertions.assertEquals(0, Day02.star1(null))
+        Assertions.assertEquals(
+            2, Day02.star1(
+                """7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9"""
+            )
+        )
     }
 
     @Test
     fun star2() {
-        Assertions.assertEquals(0, Day02.star2(null))
+        Assertions.assertEquals(
+            4, Day02.star2(
+                """7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9"""
+            )
+        )
     }
 }
