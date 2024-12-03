ab47dec1bbdb8250b79ebbe9ad680b7d4739f892
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2024-12-03 12:45:44 +0100

    2024 - day 03 :ambulance:

--- src/main/kotlin/com/github/lemfi/adventofcode/year2024/Day03.kt
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2024/Day03.kt
@@ -6,9 +6,48 @@ object Day03 {
 
     private fun String?.toData() = this ?: data(3)
 
-    fun star1(input: String?) = 0
+    fun star1(input: String?) = input.toData()
+        .toMul()
+        .sumOf { (o1, o2) -> o1 * o2 }
 
-    fun star2(input: String?) = 0
+
+    fun star2(input: String?) = input.toData()
+        .clean()
+        .toMul()
+        .sumOf { (o1, o2) -> o1 * o2 }
+
+    private fun String.toMul(): List<Pair<Int, Int>> = generateSequence(this) { remaining ->
+        if (remaining.indexOfMul() < 0) null
+        else remaining.drop(remaining.indexOfMul())
+    }
+        .mapNotNull { line ->
+            runCatching {
+                val ip1 = line.indexOf(",")
+                val ip2 = line.indexOf(")")
+
+                val a = line.take(ip1).toInt()
+                val b = line.substring(ip1 + 1, ip2).toInt()
+
+                a to b
+            }.getOrNull()
+        }
+        .toList()
+
+    private fun String.clean(): String =
+        generateSequence(this) { remaining ->
+            if (remaining.indexOfStartIgnore() < 0) null
+            else {
+                remaining
+                    .take(remaining.indexOfStartIgnore()) +
+                        remaining
+                            .drop(remaining.indexOfStartIgnore())
+                            .run { drop(indexOfStopIgnore()) }
+            }
+        }.last()
+
+    private fun String.indexOfMul() = if (contains("mul(")) indexOf("mul(") + 4 else -1
+    private fun String.indexOfStartIgnore() = if (contains("don't()")) indexOf("don't()") else -1
+    private fun String.indexOfStopIgnore() = if (contains("do()")) indexOf("do()") + 4 else Int.MAX_VALUE
 }
 
 fun main() {
--- src/test/kotlin/com/github/lemfi/adventofcode/year2024/Day03Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2024/Day03Test.kt
@@ -7,11 +7,20 @@ class Day03Test {
 
     @Test
     fun star1() {
-        Assertions.assertEquals(0, Day03.star1(null))
+        Assertions.assertEquals(
+            161,
+            Day03.star1("""xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))""")
+        )
     }
 
     @Test
     fun star2() {
-        Assertions.assertEquals(0, Day03.star2(null))
+        Assertions.assertEquals(
+            48,
+            Day03.star2(
+                """xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+"""
+            )
+        )
     }
 }
