f8eb605b8b6c7c76b211b40b4f27c05bea641ef8
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2024-12-05 09:13:49 +0100

    2024 - day 05 :white_check_mark:

--- src/main/kotlin/com/github/lemfi/adventofcode/year2024/Day05.kt
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2024/Day05.kt
@@ -6,9 +6,44 @@ object Day05 {
 
     private fun String?.toData() = this ?: data(5)
 
-    fun star1(input: String?) = 0
+    fun star1(input: String?) = input.parse()
+        .let { (rules, pages) ->
+            pages
+                .filter { it.isCorrect(rules) }
+                .sumOf { it[it.size / 2] }
+        }
 
-    fun star2(input: String?) = 0
+    fun star2(input: String?) = input.parse()
+        .let { (rules, pages) ->
+            pages
+                .filterNot { it.isCorrect(rules) }
+                .map { it.fix(rules) }
+                .sumOf { it[it.size / 2] }
+        }
+
+    private fun String?.parse() = toData()
+        .split("\n\n")
+        .let { (rules, pages) ->
+            rules.lines()
+                .map {
+                    it.split("|").let { it[0].toInt() to it[1].toInt() }
+                } to pages.lines().map { it.split(",").map { it.toInt() } }
+
+        }
+
+    private fun List<Int>.fix(rules: List<Pair<Int, Int>>): List<Int> =
+        sortedWith { a, b ->
+            when {
+                rules.any { (before, after) -> before == b && after == a } -> 1
+                else -> -1
+            }
+        }
+
+    private fun List<Int>.isCorrect(
+        rules: List<Pair<Int, Int>>
+    ) = indices.all { index ->
+        drop(index + 1).none { checkAfter -> rules.any { (before, after) -> after == this[index] && before == checkAfter } }
+    }
 }
 
 fun main() {
