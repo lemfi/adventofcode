b3f89a768d67ad1e043d123d121b830d247c6a5d
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2024-12-07 14:08:36 +0100

    2024 - day 07 :pencil2:

--- src/main/kotlin/com/github/lemfi/adventofcode/year2024/Day07.kt
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2024/Day07.kt
@@ -6,9 +6,53 @@ object Day07 {
 
     private fun String?.toData() = this ?: data(7)
 
-    fun star1(input: String?) = 0
+    fun star1(input: String?) = input
+        .parse()
+        .sumOf { (result, values) ->
+            result.check(values.drop(1), values[0], { a, b -> a + b }, { a, b -> a * b })
+        }
 
-    fun star2(input: String?) = 0
+    fun star2(input: String?) = input
+        .parse()
+        .sumOf { (result, values) ->
+            result.check(
+                values.drop(1),
+                values[0],
+                { a, b -> a + b },
+                { a, b -> a * b },
+                { a, b -> "$a$b".toLong() },
+            )
+        }
+
+    private fun String?.parse() = toData()
+        .lines()
+        .map {
+            it
+                .split(": ")
+                .let { (a, b) ->
+                    a.toLong() to b.split(" ").map(String::toLong)
+                }
+        }
+
+    private fun Long.check(values: List<Long>, currentValue: Long, vararg operations: (Long, Long) -> Long): Long =
+        when {
+
+            values.isEmpty() -> when (currentValue) {
+                this -> this
+                else -> 0
+            }
+
+            else -> operations
+                .firstOrNull { operation ->
+                    check(
+                        values.drop(1),
+                        operation(currentValue, values[0]),
+                        *operations
+                    ) == this
+                }
+                ?.let { this }
+                ?: 0
+        }
 }
 
 fun main() {
--- src/test/kotlin/com/github/lemfi/adventofcode/year2024/Day07Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2024/Day07Test.kt
@@ -7,11 +7,35 @@ class Day07Test {
 
     @Test
     fun star1() {
-        Assertions.assertEquals(0, Day07.star1(null))
+        Assertions.assertEquals(
+            3749, Day07.star1(
+                """190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20"""
+            )
+        )
     }
 
     @Test
     fun star2() {
-        Assertions.assertEquals(0, Day07.star2(null))
+        Assertions.assertEquals(
+            11387, Day07.star2(
+                """190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20"""
+            )
+        )
     }
 }
