c3bbdf5d36b6194523cec8a2dde7f67f2fd644d8
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2024-12-10 23:09:01 +0100

    2024 - day 10 :thread:

--- src/main/kotlin/com/github/lemfi/adventofcode/year2024/Day10.kt
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2024/Day10.kt
@@ -1,14 +1,42 @@
 package com.github.lemfi.adventofcode.year2024
 
 import com.github.lemfi.adventofcode.processStars
+import com.github.lemfi.adventofcode.stuff.Coordinate
+import com.github.lemfi.adventofcode.stuff.Direction4
+import com.github.lemfi.adventofcode.stuff.ReadMap.toMap
 
 object Day10 {
 
     private fun String?.toData() = this ?: data(10)
 
-    fun star1(input: String?) = 0
+    fun star1(input: String?) = input.solve { mutableSetOf() }
+    fun star2(input: String?) = input.solve { mutableListOf() }
 
-    fun star2(input: String?) = 0
+    private fun String?.solve(valid: () -> MutableCollection<Coordinate>) =
+        toData()
+            .toMap { it, _, _ -> it.digitToInt() }
+            .let { map ->
+                map
+                    .filter { it.value == 0 }
+                    .map { it.key }
+                    .sumOf { it.countValidPaths(map, valid()) }
+            }
+
+    private fun Coordinate.countValidPaths(map: Map<Coordinate, Int>, valid: MutableCollection<Coordinate>): Int {
+        val toExplore = mutableListOf(this)
+        do {
+            val current = toExplore.removeAt(toExplore.lastIndex)
+            if (map[current] == 9) valid.add(current)
+            else toExplore.addAll(
+                Direction4
+                    .entries
+                    .map { current.to(it) }
+                    .filter { next -> map[next] == map[current]!! + 1 }
+            )
+
+        } while (toExplore.isNotEmpty())
+        return valid.size
+    }
 }
 
 fun main() {
--- src/test/kotlin/com/github/lemfi/adventofcode/year2024/Day10Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2024/Day10Test.kt
@@ -6,12 +6,46 @@ import org.junit.jupiter.api.Test
 class Day10Test {
 
     @Test
-    fun star1() {
-        Assertions.assertEquals(0, Day10.star1(null))
+    fun star11() {
+        Assertions.assertEquals(
+            1, Day10.star1(
+                """0123
+1234
+8765
+9876"""
+            )
+        )
+    }
+
+    @Test
+    fun star12() {
+        Assertions.assertEquals(
+            36, Day10.star1(
+                """89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732"""
+            )
+        )
     }
 
     @Test
     fun star2() {
-        Assertions.assertEquals(0, Day10.star2(null))
+        Assertions.assertEquals(
+            81, Day10.star2(
+                """89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732"""
+            )
+        )
     }
 }
