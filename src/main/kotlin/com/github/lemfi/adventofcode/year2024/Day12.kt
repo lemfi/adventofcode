eb58a3a55717abf49386532617a4618f9291dbcc
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2024-12-12 21:44:54 +0100

    2024 - day 12 :construction:

--- src/main/kotlin/com/github/lemfi/adventofcode/year2024/Day12.kt
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2024/Day12.kt
@@ -1,14 +1,94 @@
 package com.github.lemfi.adventofcode.year2024
 
 import com.github.lemfi.adventofcode.processStars
+import com.github.lemfi.adventofcode.stuff.Coordinate
+import com.github.lemfi.adventofcode.stuff.Direction4
+import com.github.lemfi.adventofcode.stuff.ReadMap.toMap
 
 object Day12 {
 
     private fun String?.toData() = this ?: data(12)
 
-    fun star1(input: String?) = 0
+    fun star1(input: String?) = input
+        .parse()
+        .solve { perimeter(it) }
 
-    fun star2(input: String?) = 0
+    fun star2(input: String?) = input
+        .parse()
+        .solve { edges(it) }
+
+    private fun String?.parse() = toData().toMap { it, _, _ -> "$it" }
+
+    private fun Map<Coordinate, String>.solve(solver: Map<Coordinate, String>.(Collection<Coordinate>) -> Long): Long =
+        toGroups().sumOf { it.size * solver(it) }
+
+    private fun Map<Coordinate, String>.toGroups(): MutableList<Collection<Coordinate>> {
+        val coordinates = keys.toMutableList()
+        val groups = mutableListOf<Collection<Coordinate>>()
+        while (coordinates.isNotEmpty()) {
+            val coordinate = coordinates.removeFirst()
+            extractGroup(coordinate).also {
+                groups.add(it)
+                coordinates.removeAll(it)
+            }
+        }
+        return groups
+    }
+
+    private fun Map<Coordinate, String>.extractGroup(coordinate: Coordinate): Collection<Coordinate> {
+        val plant = this[coordinate]!!
+        val possibilities = mutableListOf(*coordinate.neighbors4().toTypedArray())
+        val group = mutableSetOf(coordinate)
+        while (possibilities.isNotEmpty()) {
+            val next = possibilities.removeFirst()
+            if (this[next] == plant && group.add(next)) {
+                possibilities.add(next)
+                possibilities.addAll(next.neighbors4())
+            }
+        }
+        return group
+    }
+
+    private fun Map<Coordinate, String>.perimeter(coords: Collection<Coordinate>) =
+        this[coords.first()]!!.let { plant ->
+            coords.sumOf { coordinate ->
+                coordinate.neighbors4()
+                    .count { this[it] != plant }
+                    .toLong()
+            }
+        }
+
+    private fun edges(coords: Collection<Coordinate>): Long =
+        coords.map {
+            setOf(it, it.to(Direction4.RIGHT), it.to(Direction4.DOWN), it.to(Direction4.DOWN).to(Direction4.RIGHT))
+        }.let { plantsVertices ->
+            plantsVertices
+                .flatten()
+                .toSet()
+                .sumOf { coordinate ->
+                    when {
+                        plantsVertices.count { it.contains(coordinate) } % 2 != 0 -> 1L // impair = 1 ou 3 = sommet
+                        plantsVertices.count { it.contains(coordinate) } == 2 ->
+
+                            // présente deux fois mais aucun côté adjacent
+                            // = sommet qui touche en coin deux zones,
+                            // donc on le compte deux fois, une fois pour chaque zone
+                            // merci les tests je m'en serais jamais sortie ^^ :scream:
+
+                            when {
+                                plantsVertices
+                                    .filter { it.contains(coordinate) }
+                                    .reduce { v1, v2 -> v1 intersect v2 }
+                                    .size == 1 -> 2
+
+                                else -> 0
+                            }
+
+                        else -> 0
+                    }
+                }
+        }
+
 }
 
 fun main() {
--- src/test/kotlin/com/github/lemfi/adventofcode/year2024/Day12Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2024/Day12Test.kt
@@ -6,12 +6,84 @@ import org.junit.jupiter.api.Test
 class Day12Test {
 
     @Test
-    fun star1() {
-        Assertions.assertEquals(0, Day12.star1(null))
+    fun star11() {
+        Assertions.assertEquals(
+            140, Day12.star1(
+                """AAAA
+BBCD
+BBCC
+EEEC"""
+            )
+        )
     }
 
     @Test
-    fun star2() {
-        Assertions.assertEquals(0, Day12.star2(null))
+    fun star12() {
+        Assertions.assertEquals(
+            1930, Day12.star1(
+                """RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE"""
+            )
+        )
+    }
+
+    @Test
+    fun star21() {
+        Assertions.assertEquals(
+            80, Day12.star2(
+                """AAAA
+BBCD
+BBCC
+EEEC"""
+            )
+        )
+    }
+
+    @Test
+    fun star22() {
+        Assertions.assertEquals(
+            236, Day12.star2(
+                """EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE"""
+            )
+        )
+    }
+
+    @Test
+    fun star23() {
+        Assertions.assertEquals(
+            368, Day12.star2(
+                """AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA"""
+            )
+        )
+    }
+
+    @Test
+    fun star24() {
+        Assertions.assertEquals(
+            436, Day12.star2(
+                """OOOOO
+OXOXO
+OOOOO
+OXOXO
+OOOOO"""
+            )
+        )
     }
 }
