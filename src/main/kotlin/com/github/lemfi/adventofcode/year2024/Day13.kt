8c6ee5d3bca200b322d4156e523ca80c00dd1fa3
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2024-12-14 16:10:02 +0100

    2024 - day 13 :hammer:

--- src/main/kotlin/com/github/lemfi/adventofcode/year2024/Day13.kt
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2024/Day13.kt
@@ -6,9 +6,82 @@ object Day13 {
 
     private fun String?.toData() = this ?: data(13)
 
-    fun star1(input: String?) = 0
+    fun star1(input: String?) = input
+        .toMachines()
+        .solve()
 
-    fun star2(input: String?) = 0
+
+    fun star2(input: String?) = input
+        .toMachines(10000000000000L)
+        .solve()
+
+    private fun List<Machine>.solve() = map { machine -> machine.solve().let { (a, b) -> Triple(machine, a, b) } }
+        .filter { (machine, a, b) -> Solver(a, b).isValid(machine) }
+        .sumOf { (_, a, b) ->
+            3 * a + b
+        }
+
+    private fun String?.toMachines(addition: Long = 0L) = toData().split("\n\n")
+        .map { it.lines() }
+        .map { (ba, bb, prize) ->
+            Machine(
+                Button(
+                    x = ba.substringAfter("Button A: X+").substringBefore(",").toInt(),
+                    y = ba.substringAfter(", Y+").toInt(),
+                ),
+                Button(
+                    x = bb.substringAfter("Button B: X+").substringBefore(",").toInt(),
+                    y = bb.substringAfter(", Y+").toInt(),
+                ),
+                Coordinate(
+                    x = prize.substringAfter("Prize: X=").substringBefore(",").toLong() + addition,
+                    y = prize.substringAfter(", Y=").toLong() + addition,
+                )
+            )
+        }
+
+    private fun Machine.solve(): Pair<Long, Long> = run {
+
+        val b = (prize.x * a.y - prize.y * a.x) / (b.x * a.y - b.y * a.x)
+        val a = (prize.x - b * this.b.x) / a.x
+
+        a to b
+    }
+
+    data class Solver(
+        val pressedA: Long,
+        val pressedB: Long,
+        val multiplier: Long = 1,
+    ) {
+        fun isValid(machine: Machine): Boolean =
+            isValidX(machine) and isValidY(machine)
+
+        private fun isValidX(machine: Machine): Boolean =
+            (machine.prize.x == x(machine))
+
+        private fun isValidY(machine: Machine): Boolean =
+            (machine.prize.y == y(machine))
+
+        fun x(machine: Machine) = multiplier * (pressedA * machine.a.x + pressedB * machine.b.x)
+        fun y(machine: Machine) = multiplier * (pressedA * machine.a.y + pressedB * machine.b.y)
+
+    }
+
+    data class Machine(
+        val a: Button,
+        val b: Button,
+        val prize: Coordinate
+    )
+
+    data class Button(
+        val x: Int,
+        val y: Int
+    )
+
+    data class Coordinate(
+        val x: Long,
+        val y: Long,
+    )
 }
 
 fun main() {
--- src/test/kotlin/com/github/lemfi/adventofcode/year2024/Day13Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2024/Day13Test.kt
@@ -7,11 +7,31 @@ class Day13Test {
 
     @Test
     fun star1() {
-        Assertions.assertEquals(0, Day13.star1(null))
+        Assertions.assertEquals(
+            480, Day13.star1(
+                """Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279"""
+            )
+        )
     }
 
     @Test
     fun star2() {
-        Assertions.assertEquals(0, Day13.star2(null))
+        Assertions.assertEquals(
+            82570698600470, Day13.star2(null)
+        )
     }
 }
