1ac64c9e8ef2ef4ec9117ef5f885c3dade7a7bc3
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2024-12-19 19:10:58 +0100

    2024 - day 19 :fire:

--- src/main/kotlin/com/github/lemfi/adventofcode/year2024/Day19.kt
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2024/Day19.kt
@@ -6,9 +6,34 @@ object Day19 {
 
     private fun String?.toData() = this ?: data(19)
 
-    fun star1(input: String?) = 0
+    fun star1(input: String?) = input.parse()
+        .let { (towels, designs) ->
+            designs.count { design -> design.combinations(towels) > 0 }
+        }
 
-    fun star2(input: String?) = 0
+    fun star2(input: String?) = input.parse()
+        .let { (towels, designs) ->
+            designs.sumOf { design -> design.combinations(towels) }
+        }
+
+    private fun String?.parse() = toData()
+        .split("\n\n")
+        .let { (towels, designs) -> towels.split(", ") to designs.lines() }
+
+    private fun String.combinations(towels: List<String>): Long {
+        val cache = mutableMapOf<String, Long>()
+        fun String.recursiveCombos(towels: List<String>): Long =
+            if (isBlank()) 1
+            else towels
+                .filter { startsWith(it) }
+                .sumOf { towel ->
+                    val next = drop(towel.length)
+                    cache[next] ?: next
+                        .recursiveCombos(towels.filter { it in next })
+                        .apply { cache[next] = this }
+                }
+        return recursiveCombos(towels)
+    }
 }
 
 fun main() {
--- src/test/kotlin/com/github/lemfi/adventofcode/year2024/Day19Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2024/Day19Test.kt
@@ -7,11 +7,37 @@ class Day19Test {
 
     @Test
     fun star1() {
-        Assertions.assertEquals(0, Day19.star1(null))
+        Assertions.assertEquals(
+            6, Day19.star1(
+                """r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb"""
+            )
+        )
     }
 
     @Test
     fun star2() {
-        Assertions.assertEquals(0, Day19.star2(null))
+        Assertions.assertEquals(
+            16, Day19.star2(
+                """r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb"""
+            )
+        )
     }
 }
