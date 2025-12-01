97e0a02ff0990430be81711d89d1d188fea85480
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2025-12-01 23:43:18 +0100

    2025 - day 01 :t-rex:

--- src/main/kotlin/com/github/lemfi/adventofcode/year2025/Day01.kt
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2025/Day01.kt
@@ -6,9 +6,41 @@ object Day01 {
 
     private fun String?.toData() = this ?: data(1)
 
-    fun star1(input: String?) = 0
+    fun star1(input: String?) = input.toData().lines()
+        .map { it.take(1) to it.drop(1).toInt() }
+        .fold(50 to 0) { (dial, password), (direction, steps) ->
+            when (direction) {
+                "L" -> (dial - steps).let { (if (it < 0) 100 + it else it) % 100 }
+                "R" -> (dial + steps) % 100
+                else -> error("Unknown direction $direction")
+            }
+                .let { it to if (it == 0) password + 1 else password }
+        }
+        .second
 
-    fun star2(input: String?) = 0
+    fun star2(input: String?) = input.toData().lines()
+        .map { it.take(1) to it.drop(1).toInt() }
+        .fold(50 to 0) { (dial, password), (direction, steps) ->
+
+            when (direction) {
+
+                "L" -> {
+                    val newDial = (dial - steps % 100).let { if (it < 0) 100 + it else it }
+                    val hits = ((steps / 100) + if (dial != 0 && steps % 100 >= dial) 1 else 0)
+                    newDial to password + hits
+                }
+
+                "R" -> {
+                    val newDial = (dial + steps % 100) % 100
+                    val hits = ((steps / 100) + if (dial != 0 && steps % 100 >= 100 - dial) 1 else 0)
+                    newDial to password + hits
+                }
+
+                else -> error("Unknown direction $direction")
+            }
+        }
+        .second
+
 }
 
 fun main() {
--- src/test/kotlin/com/github/lemfi/adventofcode/year2025/Day01Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2025/Day01Test.kt
@@ -7,11 +7,29 @@ class Day01Test {
 
     @Test
     fun star1() {
-        Assertions.assertEquals(0, Day01.star1(null))
+        Assertions.assertEquals(3, Day01.star1("""L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82"""))
     }
 
     @Test
     fun star2() {
-        Assertions.assertEquals(0, Day01.star2(null))
+        Assertions.assertEquals(6, Day01.star2("""L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82"""))
     }
 }
