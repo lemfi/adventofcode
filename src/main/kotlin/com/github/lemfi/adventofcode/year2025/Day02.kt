ca2d430ae99ddd2690933b55e9ad0c811b5f211a
Fiona Le Montreer <fiona.le.montreer@gmail.com>
2025-12-02 21:39:54 +0100

    2025 - day 02 :wrench:

--- src/main/kotlin/com/github/lemfi/adventofcode/year2025/Day02.kt
+++ src/main/kotlin/com/github/lemfi/adventofcode/year2025/Day02.kt
@@ -1,14 +1,43 @@
 package com.github.lemfi.adventofcode.year2025
 
 import com.github.lemfi.adventofcode.processStars
+import kotlin.math.pow
 
 object Day02 {
 
     private fun String?.toData() = this ?: data(2)
 
-    fun star1(input: String?) = 0
+    fun star1(input: String?) = input.toData()
+        .split(",")
+        .map { it.split("-") }
+        .map { (a, b) -> a.toLong() to b.toLong() }
+        .sumOf { (a, b) ->
+            (a..b).sumOf {
+                val pow = (10.0.pow("$it".length / 2)).toLong()
+                if (it / pow == it % pow) it else 0L
+            }
+        }
 
-    fun star2(input: String?) = 0
+    fun star2(input: String?) = input.toData()
+        .split(",")
+        .map { it.split("-") }
+        .sumOf { (a, b) ->
+            (a.toLong()..b.toLong()).sumOf { number -> if (number.hasRepeatingPattern()) number else 0L }
+        }
+}
+
+private fun Long.hasRepeatingPattern(): Boolean {
+    val len = toString().length
+    return (1..len / 2)
+        .filter { len % it == 0 }
+        .any { k ->
+            val pow = 10.0.pow(k).toLong()
+            val reference = this % pow
+            generateSequence(this) { n -> n / pow }
+                .takeWhile { it > 0 }
+                .all { n ->
+                    n % pow == reference }
+        }
 }
 
 fun main() {
--- src/test/kotlin/com/github/lemfi/adventofcode/year2025/Day02Test.kt
+++ src/test/kotlin/com/github/lemfi/adventofcode/year2025/Day02Test.kt
@@ -7,11 +7,26 @@ class Day02Test {
 
     @Test
     fun star1() {
-        Assertions.assertEquals(0, Day02.star1(null))
+        Assertions.assertEquals(1227775554, Day02.star1("""11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"""))
     }
 
     @Test
     fun star2() {
-        Assertions.assertEquals(0, Day02.star2(null))
+        Assertions.assertEquals(4174379265L, Day02.star2("""11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"""))
+    }
+
+    @Test
+    fun star21() {
+        Assertions.assertEquals(33L, Day02.star2("""11-22"""))
+    }
+
+    @Test
+    fun star22() {
+        Assertions.assertEquals(210L, Day02.star2("""95-115"""))
+    }
+
+    @Test
+    fun star23() {
+        Assertions.assertEquals(999L + 1010L, Day02.star2("""998-1012"""))
     }
 }
